5c1bb87f470325348f5b995f343191f5d2b5b3a7
Knut Nygaard <knut.nygaard@nav.no>
2018-04-04 13:03:22 +0200

    Tatt inn klient for å hente innkommende journalpost fra joark
    SYFOUTV-2025

---
+++ src/main/java/no/nav/syfo/domain/SyfoException.java
@@ -0,0 +1,6 @@
+package no.nav.syfo.domain;
+
+public class SyfoException extends RuntimeException {
+
+    public String message;
+}
---
+++ src/main/java/no/nav/syfo/ws/WsClient.java
@@ -0,0 +1,28 @@
+package no.nav.syfo.ws;
+
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.frontend.ClientProxy;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.PhaseInterceptor;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class WsClient<T> {
+
+
+
+    @SuppressWarnings("unchecked")
+    public T createPort(String serviceUrl, Class<?> portType, PhaseInterceptor<? extends Message>... interceptors) {
+        JaxWsProxyFactoryBean jaxWsProxyFactoryBean = new JaxWsProxyFactoryBean();
+        jaxWsProxyFactoryBean.setServiceClass(portType);
+        jaxWsProxyFactoryBean.setAddress(Objects.requireNonNull(serviceUrl));
+        T port = (T) jaxWsProxyFactoryBean.create();
+        Client client = ClientProxy.getClient(port);
+        Arrays.stream(interceptors).forEach(client.getOutInterceptors()::add);
+        STSClientConfig.configureRequestSamlToken(port);
+        return port;
+    }
+
+}
