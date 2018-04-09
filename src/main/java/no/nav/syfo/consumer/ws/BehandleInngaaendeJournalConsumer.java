69c41d3648f7c1f73b6ca1d14e8a0453d109e857
Knut Nygaard <knut.nygaard@nav.no>
2018-04-09 14:44:24 +0200

    Fjernet ubrukte imports

--- src/main/java/no/nav/syfo/consumer/ws/BehandleInngaaendeJournalConsumer.java
+++ src/main/java/no/nav/syfo/consumer/ws/BehandleInngaaendeJournalConsumer.java
@@ -1,15 +1,8 @@
 package no.nav.syfo.consumer.ws;
 
 import lombok.extern.slf4j.Slf4j;
-import no.nav.syfo.domain.InngaaendeJournalpost;
-import no.nav.syfo.domain.SyfoException;
 import no.nav.tjeneste.virksomhet.behandle.inngaaende.journal.v1.BehandleInngaaendeJournalV1;
-import no.nav.tjeneste.virksomhet.behandle.inngaaende.journal.v1.WSOppdaterJournalpostRequest;
-import no.nav.tjeneste.virksomhet.inngaaende.journal.v1.*;
 import org.springframework.stereotype.Component;
-
-import static no.nav.syfo.mappers.WS2InngaaendeJournalpostMapper.ws2InngaaendeJouralpost;
-import static no.nav.syfo.util.MapUtil.map;
 
 @Component
 @Slf4j
