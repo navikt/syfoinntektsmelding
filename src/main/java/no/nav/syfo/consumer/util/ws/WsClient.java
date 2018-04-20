e2382ee4309b38ec1d8d5a81048ccf7f1bb5cbeb
Martin Solheim <martin.solheim@nav.no>
2018-04-20 14:57:51 +0200

    Legger til LogErrorHandler for soap errors på ws klienten
    STFOUTV-1649

--- src/main/java/no/nav/syfo/consumer/util/ws/WsClient.java
+++ src/main/java/no/nav/syfo/consumer/util/ws/WsClient.java
@@ -6,7 +6,10 @@ import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.PhaseInterceptor;
 
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.handler.Handler;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class WsClient<T> {
@@ -14,11 +17,12 @@ public class WsClient<T> {
 
 
     @SuppressWarnings("unchecked")
-    public T createPort(String serviceUrl, Class<?> portType, PhaseInterceptor<? extends Message>... interceptors) {
+    public T createPort(String serviceUrl, Class<?> portType, List<Handler> handlers, PhaseInterceptor<? extends Message>... interceptors) {
         JaxWsProxyFactoryBean jaxWsProxyFactoryBean = new JaxWsProxyFactoryBean();
         jaxWsProxyFactoryBean.setServiceClass(portType);
         jaxWsProxyFactoryBean.setAddress(Objects.requireNonNull(serviceUrl));
         T port = (T) jaxWsProxyFactoryBean.create();
+        ((BindingProvider) port).getBinding().setHandlerChain(handlers);
         Client client = ClientProxy.getClient(port);
         Arrays.stream(interceptors).forEach(client.getOutInterceptors()::add);
         STSClientConfig.configureRequestSamlToken(port);
