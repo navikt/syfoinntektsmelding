942ddddb1b509171032db07219775bb0246997a9
Martin Solheim <martin.solheim@nav.no>
2018-05-04 15:49:07 +0200

    Legger på test-endepunkt for hent sykepengesøknad fra database

--- src/main/java/no/nav/syfo/web/selftest/SelftestController.java
+++ src/main/java/no/nav/syfo/web/selftest/SelftestController.java
@@ -1,10 +1,15 @@
 package no.nav.syfo.web.selftest;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.syfo.domain.Sykepengesoknad;
+import no.nav.syfo.repository.SykepengesoknadDAO;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
 
 @Slf4j
 @RestController
@@ -12,6 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class SelftestController {
     private static final String APPLICATION_LIVENESS = "Application is alive!";
     private static final String APPLICATION_READY = "Application is ready!";
+
+    private final SykepengesoknadDAO sykepengesoknadDAO;
+
+    @Inject
+    public SelftestController(SykepengesoknadDAO sykepengesoknadDAO) {
+        this.sykepengesoknadDAO = sykepengesoknadDAO;
+    }
 
     @ResponseBody
     @RequestMapping(value = "/isAlive", produces = MediaType.TEXT_PLAIN_VALUE)
@@ -23,6 +35,12 @@ public class SelftestController {
     @RequestMapping(value = "/isReady", produces = MediaType.TEXT_PLAIN_VALUE)
     public String isReady() {
         return APPLICATION_READY;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/hetSykepengesoknad/{journalpostid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Sykepengesoknad hentSykepengesoknad(@PathVariable String journalpostid) {
+        return sykepengesoknadDAO.finnSykepengesoknad(journalpostid).get();
     }
 
 }
