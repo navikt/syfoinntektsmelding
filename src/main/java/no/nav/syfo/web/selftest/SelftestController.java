853243a8288c13ee4fac4c30c5cf66d2f6bda0cd
Martin Solheim <martin.solheim@nav.no>
2018-05-23 13:37:12 +0200

    Legger til endepunkt for å teste behandleInntektsmelding
    SYFOUTV-2095

--- src/main/java/no/nav/syfo/web/selftest/SelftestController.java
+++ src/main/java/no/nav/syfo/web/selftest/SelftestController.java
@@ -1,12 +1,14 @@
 package no.nav.syfo.web.selftest;
 
 import lombok.extern.slf4j.Slf4j;
-import no.nav.syfo.consumer.mq.InntektsmeldingConsumer;
+import no.nav.syfo.domain.Inntektsmelding;
+import no.nav.syfo.service.SaksbehandlingService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
 
 @Slf4j
 @RestController
@@ -15,7 +17,12 @@ public class SelftestController {
     private static final String APPLICATION_LIVENESS = "Application is alive!";
     private static final String APPLICATION_READY = "Application is ready!";
 
-    InntektsmeldingConsumer inntektsmeldingConsumer;
+    private SaksbehandlingService saksbehandlingService;
+
+    @Inject
+    public SelftestController(SaksbehandlingService saksbehandlingService) {
+        this.saksbehandlingService = saksbehandlingService;
+    }
 
     @ResponseBody
     @RequestMapping(value = "/isAlive", produces = MediaType.TEXT_PLAIN_VALUE)
@@ -30,8 +37,16 @@ public class SelftestController {
     }
 
     @ResponseBody
-    @RequestMapping(value = "/leggFakeInntektsmelingPaKo/{journalpostid}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public void hentSykepengesoknad(@PathVariable String journalpostid) {
+    @RequestMapping(value = "/behandleFakeInntektsmelding/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String behandleFakeInntektsmelding() {
+        log.info("Behandler fake inntektsmelding!");
+
+        Inntektsmelding inntektsmelding = Inntektsmelding.builder()
+                .arbeidsgiverOrgnummer("orgnummer")
+                .journalpostId("journalpostid")
+                .fnr("2202690062").build();
+
+        return saksbehandlingService.behandleInntektsmelding(inntektsmelding);
     }
 
 }
