117b1dd2a948a2eb51a719f0f8b1a72862ef9412
Anders Østby <anders.ostby@nav.no>
2018-08-08 14:41:31 +0200

    Revert "Forhindrer logspam fra atomikos"
    This reverts commit caf0ed8

--- src/main/java/no/nav/syfo/config/ApplicationConfig.java
+++ src/main/java/no/nav/syfo/config/ApplicationConfig.java
@@ -1,20 +1,10 @@
 package no.nav.syfo.config;
 
-import org.flywaydb.core.Flyway;
-import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.jta.JtaTransactionManager;
 
 
 @Configuration
 @EnableTransactionManagement
 public class ApplicationConfig {
-    // Sørger for at flyway migrering skjer etter at JTA transaction manager er ferdig satt opp av Spring.
-    // Forhindrer WARNING: transaction manager not running? loggspam fra Atomikos.
-    @Bean
-    FlywayMigrationStrategy flywayMigrationStrategy(final JtaTransactionManager jtaTransactionManager) {
-        return Flyway::migrate;
-    }
 }
