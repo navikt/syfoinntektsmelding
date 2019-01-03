d2abeef2ca4954afed5e9abd8495662041faa9f8
Knut Nygaard <knutnyg@gmail.com>
2019-01-03 10:04:28 +0100

    Testendepunkt for aktor

---
+++ src/main/java/no/nav/syfo/repository/InntektsmeldingDAO.java
@@ -0,0 +1,13 @@
+package no.nav.syfo.repository;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@Transactional
+@Repository
+public class InntektsmeldingDAO {
+}
--- src/main/java/no/nav/syfo/web/selftest/SelftestController.java
+++ src/main/java/no/nav/syfo/web/selftest/SelftestController.java
@@ -1,15 +1,10 @@
 package no.nav.syfo.web.selftest;
 
 import lombok.extern.slf4j.Slf4j;
-import no.nav.syfo.domain.Inntektsmelding;
-import no.nav.syfo.service.SaksbehandlingService;
+import no.nav.syfo.consumer.rest.AktorConsumer;
+import no.nav.syfo.consumer.rest.TokenConsumer;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.inject.Inject;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -17,6 +12,18 @@ import javax.inject.Inject;
 public class SelftestController {
     private static final String APPLICATION_LIVENESS = "Application is alive!";
     private static final String APPLICATION_READY = "Application is ready!";
+
+    private AktorConsumer aktorConsumer;
+
+    public SelftestController(AktorConsumer aktorConsumer) {
+        this.aktorConsumer = aktorConsumer;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/aktor/{fnr}")
+    public String aktor(@PathVariable String fnr) {
+        return aktorConsumer.getAktorId(fnr);
+    }
 
     @ResponseBody
     @RequestMapping(value = "/isAlive", produces = MediaType.TEXT_PLAIN_VALUE)
