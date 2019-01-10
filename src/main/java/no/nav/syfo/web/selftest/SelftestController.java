8c9df258b4b376bd1c47756495a577331941ef3a
Knut Nygaard <knutnyg@gmail.com>
2019-01-10 13:53:48 +0100

    Fjerner testendepunkt fra selftest

--- src/main/java/no/nav/syfo/web/selftest/SelftestController.java
+++ src/main/java/no/nav/syfo/web/selftest/SelftestController.java
@@ -1,7 +1,6 @@
 package no.nav.syfo.web.selftest;
 
 import lombok.extern.slf4j.Slf4j;
-import no.nav.syfo.consumer.rest.aktor.AktorConsumer;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,12 +13,6 @@ public class SelftestController {
     private static final String APPLICATION_LIVENESS = "Application is alive!";
     private static final String APPLICATION_READY = "Application is ready!";
 
-    private final AktorConsumer aktorConsumer;
-
-    public SelftestController(AktorConsumer aktorConsumer) {
-        this.aktorConsumer = aktorConsumer;
-    }
-
     @ResponseBody
     @RequestMapping(value = "/isAlive", produces = MediaType.TEXT_PLAIN_VALUE)
     public String isAlive() {
@@ -30,11 +23,5 @@ public class SelftestController {
     @RequestMapping(value = "/isReady", produces = MediaType.TEXT_PLAIN_VALUE)
     public String isReady() {
         return APPLICATION_READY;
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/test", produces = MediaType.TEXT_PLAIN_VALUE)
-    public String test() {
-        return aktorConsumer.getAktorId("18026902092");
     }
 }
