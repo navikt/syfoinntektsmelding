337e3d9c1e33f19fe2cff8ad13fcc0e5c0d88d65
Knut Nygaard <knutnyg@gmail.com>
2019-03-07 15:54:37 +0100

    Legger på noe logging

--- src/main/kotlin/no/nav/syfo/service/EksisterendeSakService.kt
+++ src/main/kotlin/no/nav/syfo/service/EksisterendeSakService.kt
@@ -1,5 +1,6 @@
 package no.nav.syfo.service
 
+import log
 import no.nav.syfo.consumer.rest.EksisterendeSakConsumer
 import no.nav.syfo.consumer.SakConsumer
 import org.springframework.stereotype.Service
@@ -9,9 +10,13 @@ class EksisterendeSakService(
         val eksisterendeSakConsumer: EksisterendeSakConsumer,
         val sakConsumer: SakConsumer) {
 
+    val log = log()
+
     fun finnEksisterendeSak(aktorId: String, orgnummer: String?): String? {
         val maybeSakFraSyfoservice = eksisterendeSakConsumer.finnEksisterendeSaksId(aktorId, orgnummer).orElse(null)
         val maybeSakFraSyfogsak = sakConsumer.finnSisteSak(aktorId)
+
+        log.info("Sak fra service: {}, sak fra syfogsak: {}", maybeSakFraSyfoservice, maybeSakFraSyfogsak)
 
         return listOfNotNull(maybeSakFraSyfogsak, maybeSakFraSyfoservice)
                 .sortedDescending()
