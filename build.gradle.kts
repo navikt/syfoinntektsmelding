d3c82e2537a462d3cd0909da94f94c73e0769f98
Aleksander Andresen <aleksander.andresen@nav.no>
2019-10-25 12:45:20 +0200

    Prøver å legge til saaj

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,10 +73,12 @@ dependencies {
     implementation("javax.inject:javax.inject:1")
 
     implementation("jakarta.activation:jakarta.activation-api:1.2.1")
-    implementation("com.sun.activation:javax.activation:1.2.0")
     implementation("jakarta.xml.bind:jakarta.xml.bind-api:2.3.1")
     implementation("jakarta.xml.ws:jakarta.xml.ws-api:2.3.2")
     implementation("jakarta.ws.rs:jakarta.ws.rs-api:2.1.6")
+
+    implementation("com.sun.activation:javax.activation:1.2.0")
+    implementation("com.sun.xml.messaging.saaj:saaj-impl:1.5.1")
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.41")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.41")
