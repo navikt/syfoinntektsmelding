6905d6ac78fffa8f6cd629438412193e39b74374
Morten Laukvik <morten.laukvik@nav.no>
2019-11-14 14:11:37 +0100

    POH-9: Legger til parametere for at JPA skal kunne starte

--- src/main/java/no/nav/syfo/config/ApplicationConfig.java
+++ src/main/java/no/nav/syfo/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package no.nav.syfo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -10,9 +11,10 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
+@EnableJpaRepositories("no.nav.syfo")
 public class ApplicationConfig {
 
-    @Bean
+    @Bean(name = "transactionManager")
     public DataSourceTransactionManager datasourceTransactionManager(DataSource dataSource) {
         DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
         dataSourceTransactionManager.setDataSource(dataSource);
