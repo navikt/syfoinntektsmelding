eb83dc98340623fb462f8762cdd2a1b0f7451d8e
Aleksander Andresen <aleksander.andresen@nav.no>
2020-01-15 12:17:59 +0100

    Tweaker flyway oppsett

--- src/main/java/no/nav/syfo/config/FlywayConfig.java
+++ src/main/java/no/nav/syfo/config/FlywayConfig.java
@@ -1,6 +1,8 @@
 package no.nav.syfo.config;
 
 import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
@@ -12,16 +14,19 @@ import javax.sql.DataSource;
 @Configuration
 @ConditionalOnProperty(prefix = "spring.flyway", name = "enabled", matchIfMissing = true)
 public class FlywayConfig {
-
-    @Value("${vault.postgres.role}") String role;
+    private static final Logger LOGGER = LoggerFactory.getLogger(FlywayConfig.class.getName());
+    @Value("${vault.postgres.role}")
+    String role;
 
     @Bean
     Flyway flyway(DataSource dataSource) {
-        if (role == null || role.equalsIgnoreCase("")){
+        if (role == null || role.isBlank()) {
+            LOGGER.warn("Fant ingen role for flyway migrering");
             return Flyway.configure().dataSource(dataSource).baselineOnMigrate(true).cleanDisabled(true).schemas().load();
         }
+        LOGGER.info("Setter role til: {}", role);
         return Flyway.configure().initSql(
-            String.format("SET ROLE %s;", role)
+            String.format("SET ROLE \"%s\"", role)
         ).dataSource(dataSource).baselineOnMigrate(true).cleanDisabled(true).schemas().load();
     }
 
