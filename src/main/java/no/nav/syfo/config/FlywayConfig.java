33b52db7857a351eb0be17ceefc225fd22ae1fac
Morten Laukvik <morten.laukvik@nav.no>
2020-01-15 09:46:59 +0100

    Oppgrader flyway og sett database rolle ved migrering

--- src/main/java/no/nav/syfo/config/FlywayConfig.java
+++ src/main/java/no/nav/syfo/config/FlywayConfig.java
@@ -1,10 +1,13 @@
 package no.nav.syfo.config;
 
 import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import javax.sql.DataSource;
 
 @Configuration
@@ -13,12 +16,13 @@ public class FlywayConfig {
 
     @Bean
     Flyway flyway(DataSource dataSource) {
-        Flyway flyway = new Flyway();
-        flyway.setDataSource(dataSource);
-        flyway.setBaselineOnMigrate(true);
-        flyway.setCleanDisabled(true);
-        flyway.setSchemas();
-        return flyway;
+        return Flyway.configure().dataSource(dataSource).baselineOnMigrate(true).cleanDisabled(true).schemas().load();
+    }
+
+    @Bean
+    public FlywayConfigurationCustomizer flywayConfig( @Value("${vault.postgres.role}") String role) {
+        return c -> c
+            .initSql(String.format("SET ROLE \"%s\"", role));
     }
 
     @Bean
