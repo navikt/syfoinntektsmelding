e3e85151a35b2a454613438ecf1b21f34a67aa24
Morten Laukvik <morten.laukvik@nav.no>
2020-01-20 13:15:12 +0100

    Vault logging

--- src/main/java/no/nav/syfo/config/VaultHikariConfig.java
+++ src/main/java/no/nav/syfo/config/VaultHikariConfig.java
@@ -2,20 +2,14 @@ package no.nav.syfo.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import lombok.Data;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.syfo.ClusterAwareSpringProfileResolver;
 import no.nav.vault.jdbc.hikaricp.HikariCPVaultUtil;
-import no.nav.vault.jdbc.hikaricp.VaultError;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
@@ -34,18 +28,23 @@ public class VaultHikariConfig {
     }
 
     private String finnMountPath() {
-        String naisCluster = ClusterAwareSpringProfileResolver.profiles()[0];
-        if (naisCluster == null || naisCluster.isEmpty()){
-            return "postgresql/preprod-fss";
-        }
-        if ("dev-fss".equals(naisCluster)) {
-            return "postgresql/preprod-fss";
-        }
-        return "postgresql/" + naisCluster;
+//        String naisCluster = ClusterAwareSpringProfileResolver.profiles()[0];
+//        if (naisCluster == null || naisCluster.isEmpty()){
+//            return "postgresql/preprod-fss";
+//        }
+//        if ("dev-fss".equals(naisCluster)) {
+//            return "postgresql/preprod-fss";
+//        }
+//        return "postgresql/" + naisCluster;
+        return "postgresql/preprod-fss";
     }
 
     @SneakyThrows
     private HikariDataSource dataSource(String user) {
+        log.info("JdbdUrl", jdbcUrl);
+        log.info("user", user);
+        log.info("finnMountPath", finnMountPath());
+        log.info("dbRole", dbRole(user));
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(jdbcUrl);
         config.setMaximumPoolSize(3);
