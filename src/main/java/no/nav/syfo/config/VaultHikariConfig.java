8b344bf4e2d8fdfbc7de8a74cca2fd4ef41a5fe7
Morten Laukvik <morten.laukvik@nav.no>
2020-01-21 14:01:41 +0100

    Default til vault

--- src/main/java/no/nav/syfo/config/VaultHikariConfig.java
+++ src/main/java/no/nav/syfo/config/VaultHikariConfig.java
@@ -7,21 +7,22 @@ import no.nav.vault.jdbc.hikaricp.HikariCPVaultUtil;
 import no.nav.vault.jdbc.hikaricp.VaultError;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Slf4j
 @EnableJpaRepositories("no.nav.syfo.repository")
+@Configuration
 public class VaultHikariConfig {
 
     @Value("${vault.enabled:true}")
     public boolean enabled = true;
     @Value("${vault.backend}")
     public String databaseBackend;
-    @Value("${vault.role}")
+    @Value("${vault.role:syfoinntektsmelding-user}")
     public String databaseRole;
-    @Value("${vault.admin}")
+    @Value("${vault.admin:syfoinntektsmelding-admin}")
     public String databaseAdminrole;
 
     @Bean
