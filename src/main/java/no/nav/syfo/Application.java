06d72936d34c330f934b8cca3babf15eec0e7d37
Morten Laukvik <morten.laukvik@nav.no>
2020-01-21 12:53:28 +0100

    Fjern clusteraware

--- src/main/java/no/nav/syfo/Application.java
+++ src/main/java/no/nav/syfo/Application.java
@@ -8,7 +8,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class Application {
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)
-            .profiles(ClusterAwareSpringProfileResolver.profiles())
             .main(Application.class)
             .run(args);
     }
