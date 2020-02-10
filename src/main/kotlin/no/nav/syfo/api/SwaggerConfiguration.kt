373ad0bedda49bdc05d1e869c60f34005007bae6
Morten Laukvik <laukvik@users.noreply.github.com>
2020-02-10 09:49:31 +0100

    Rett test som har vært disablet (#68)

--- src/main/kotlin/no/nav/syfo/api/SwaggerConfiguration.kt
+++ src/main/kotlin/no/nav/syfo/api/SwaggerConfiguration.kt
@@ -2,6 +2,7 @@ package no.nav.syfo.api
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.spi.DocumentationType
@@ -10,10 +11,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
 @EnableSwagger2
+@Profile(value = ["remote", "local"])
 class SwaggerConfiguration {
 
     @Bean
-    open fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
+    fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
         .select()
         .apis(RequestHandlerSelectors.basePackage("no.nav.syfo.api"))
         .paths(PathSelectors.any())
