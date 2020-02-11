6e7bfcb342da1c9cf8741576f81c236c9fc82199
Aleksander Andresen <aleksander.andresen@nav.no>
2020-02-11 11:13:10 +0100

    Refaktorer historikk

--- src/main/kotlin/no/nav/syfo/behandling/Historikk.kt
+++ src/main/kotlin/no/nav/syfo/behandling/Historikk.kt
@@ -3,16 +3,16 @@ package no.nav.syfo.behandling
 import no.nav.syfo.dto.FeiletEntitet
 import java.time.LocalDateTime
 
+const val ANTALL_DAGER_FØR_ARKIVERING = 7L
+
 data class Historikk(val arkivReferanse: String, val feiletList: List<FeiletEntitet>) {
-
-    val ANTALL_DAGER_FØR_ARKIVERING = 7
-
     fun skalArkiveresForDato(dato: LocalDateTime = LocalDateTime.now()): Boolean {
-        if (feiletList.isEmpty()){
-            return false
-        }
-        val fe = feiletList.sortedByDescending { feiletEntitet -> feiletEntitet.tidspunkt }.last()
-        return fe.tidspunkt.isBefore(dato.minusDays(ANTALL_DAGER_FØR_ARKIVERING.toLong()))
+        return skalArkiveresForDato(dato, feiletList)
     }
+}
 
+fun skalArkiveresForDato(dato: LocalDateTime, feiletList: List<FeiletEntitet>): Boolean {
+    return feiletList.any { feil ->
+        feil.tidspunkt.isBefore(dato.minusDays(ANTALL_DAGER_FØR_ARKIVERING))
+    }
 }
