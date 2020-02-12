5ad58777cdb48f4576e1c0d75e06c77d474d331f
Morten Laukvik <morten.laukvik@nav.no>
2020-02-12 10:27:26 +0100

    Forenkle koden. Kjør kun en Application. Fjern unødvendig annotering.

--- src/test/kotlin/no/nav/syfo/repository/FeiletRepositoryTest.kt
+++ src/test/kotlin/no/nav/syfo/repository/FeiletRepositoryTest.kt
@@ -1,7 +1,6 @@
 package no.nav.syfo.repository
 
 
-import no.nav.syfo.LocalApplication
 import no.nav.syfo.behandling.Feiltype
 import no.nav.syfo.dto.FeiletEntitet
 import org.assertj.core.api.Assertions.assertThat
@@ -9,19 +8,15 @@ import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
 import java.time.LocalDateTime
 
 @RunWith(SpringRunner::class)
 @DataJpaTest
-@OverrideAutoConfiguration(enabled = true)
 @TestPropertySource(locations = ["classpath:application-test.properties"])
-@ContextConfiguration(classes = [LocalApplication::class])
 open class FeiletRepositoryTest {
     @Autowired
     private lateinit var entityManager: TestEntityManager
