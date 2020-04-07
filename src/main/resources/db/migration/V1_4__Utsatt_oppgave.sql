0f3cd9f57f03d3c2854e8e18f1d77cc5fd9afa90
Knut Nygaard <knut.nygaard@nav.no>
2020-04-07 15:50:28 +0200

    InntektsmeldingsId er unik i utsattoppgave-tabellen
    Co-authored-by: Stephen Ramthun <stephen.ramthun@nav.no>

--- src/main/resources/db/migration/V1_4__Utsatt_oppgave.sql
+++ src/main/resources/db/migration/V1_4__Utsatt_oppgave.sql
@@ -1,6 +1,6 @@
 create table UTSATT_OPPGAVE (
     OPPGAVE_ID serial not null primary key,
-    INNTEKTSMELDING_ID VARCHAR(100) not null,
+    INNTEKTSMELDING_ID VARCHAR(100) unique not null,
     ARKIVREFERANSE VARCHAR(100) not null,
     FNR VARCHAR(50) not null,
     AKTOR_ID VARCHAR(50) not null,
