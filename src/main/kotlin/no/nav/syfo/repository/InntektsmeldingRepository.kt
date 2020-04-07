524a14889693fbb7d731907039799ce160ac474f
Aleksander Andresen <caeses@gmail.com>
2020-04-07 15:40:17 +0200

    Muliggjør sletting av gamle inntektsmeldinger

--- src/main/kotlin/no/nav/syfo/repository/InntektsmeldingRepository.kt
+++ src/main/kotlin/no/nav/syfo/repository/InntektsmeldingRepository.kt
@@ -3,10 +3,15 @@ package no.nav.syfo.repository
 import no.nav.syfo.dto.InntektsmeldingEntitet
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 
 @Repository
 interface InntektsmeldingRepository : JpaRepository<InntektsmeldingEntitet, String> {
 
     fun findByAktorId(aktoerId: String): List<InntektsmeldingEntitet>
+
+    fun findFirst100ByBehandletBefore(førDato: LocalDateTime): List<InntektsmeldingEntitet>
+
+    fun deleteByBehandletBefore(førDato: LocalDateTime): List<InntektsmeldingEntitet>
 
 }
