00a635ecf4de1179b0ab5b84878a7c47c393fefc
Aleksander Andresen <aleksander.andresen@nav.no>
2020-04-08 14:50:20 +0200

    Slett gamle inntektsmelding (#80)
    * Muliggjør sletting av gamle inntektsmeldinger
    
    * Kjør sletting automatisk hver dag
    
    * Logg hvor mange inntektsmeldinger som slettes
    
    * Endre navn og legg til kommentar
    
    * Endre navn på test

--- src/main/java/no/nav/syfo/config/ApplicationConfig.java
+++ src/main/java/no/nav/syfo/config/ApplicationConfig.java
@@ -5,12 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 
 @Configuration
+@EnableScheduling
 @EnableTransactionManagement()
 @EnableJpaRepositories("no.nav.syfo.repository")
 public class ApplicationConfig {
--- src/main/kotlin/no/nav/syfo/repository/InntektsmeldingService.kt
+++ src/main/kotlin/no/nav/syfo/repository/InntektsmeldingService.kt
@@ -1,25 +1,32 @@
 package no.nav.syfo.repository
 
+import log
 import lombok.extern.slf4j.Slf4j
 import no.nav.inntektsmelding.kontrakt.serde.JacksonJsonConfig
 import no.nav.syfo.domain.inntektsmelding.Inntektsmelding
 import no.nav.syfo.dto.InntektsmeldingEntitet
 import no.nav.syfo.mapping.toInntektsmelding
 import no.nav.syfo.mapping.toInntektsmeldingEntitet
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import javax.transaction.Transactional
 
 @Service
 @Slf4j
-class InntektsmeldingService (
-    private val repository: InntektsmeldingRepository
+class InntektsmeldingService(
+    private val repository: InntektsmeldingRepository,
+    @Value("\${inntektsmelding.lagringstid.maneder:3}") val lagringstidMåneder: Int
 ) {
 
     val objectMapper = JacksonJsonConfig.objectMapperFactory.opprettObjectMapper()
+    val log = log()
+
 
     fun finnBehandledeInntektsmeldinger(aktoerId: String): List<Inntektsmelding> {
         val liste = repository.findByAktorId(aktoerId)
-        return liste.map{ InntektsmeldingMeta -> toInntektsmelding(InntektsmeldingMeta) }
+        return liste.map { InntektsmeldingMeta -> toInntektsmelding(InntektsmeldingMeta) }
     }
 
     @Transactional(Transactional.TxType.REQUIRED)
@@ -30,6 +37,17 @@ class InntektsmeldingService (
         dto.sakId = saksId
         dto.data = mapString(inntektsmelding)
         return repository.saveAndFlush(dto)
+    }
+
+    // Sekund, Minutt, Time, Dag, Måned, Ukedag
+    @Scheduled(cron = "0 0 4 * * *")
+    @Transactional(Transactional.TxType.REQUIRED)
+    @org.springframework.transaction.annotation.Transactional("transactionManager")
+    fun slettInntektsmeldingerEldreEnnKonfigurertMåneder() {
+        val konfigurertAntallMånederSiden = LocalDate.now().minusMonths(lagringstidMåneder.toLong()).atStartOfDay()
+        log.info("Sletter alle inntektsmeldinger før $konfigurertAntallMånederSiden")
+        val antallSlettet = repository.deleteByBehandletBefore(konfigurertAntallMånederSiden)
+        log.info("Slettet $antallSlettet inntektsmeldinger")
     }
 
     fun mapString(inntektsmelding: Inntektsmelding): String {
