626e050ad448136968046a4f4e20b553a98e61f5
Knut Nygaard <knut.nygaard@nav.no>
2020-04-14 09:23:05 +0200

    Oppdatert deployfiler for 2 moduler
    Co-authored-by: Stephen Ramthun <stephen.ramthun@nav.no>

--- syfoinntektsmelding-job/build.gradle.kts
+++ syfoinntektsmelding-job/build.gradle.kts
@@ -20,8 +20,8 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_12
+    targetCompatibility = JavaVersion.VERSION_12
 }
 
 tasks.withType<KotlinCompile> {
