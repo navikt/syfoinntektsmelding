81869ba93d23585b8e05d3e73eff19519cee5a5f
Morten Laukvik <morten.laukvik@nav.no>
2020-12-16 13:31:41 +0100

    POH-211: Bruk helsesjekk mot database

--- src/main/java/no/nav/syfo/web/selftest/SelftestController.java
+++ src/main/java/no/nav/syfo/web/selftest/SelftestController.java
@@ -1,10 +1,12 @@
 package no.nav.syfo.web.selftest;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Slf4j
 @RestController
@@ -13,15 +15,24 @@ public class SelftestController {
     private static final String APPLICATION_LIVENESS = "Application is alive!";
     private static final String APPLICATION_READY = "Application is ready!";
 
+    @Autowired
+    private JdbcTemplate template;
+
     @ResponseBody
     @RequestMapping(value = "/isAlive", produces = MediaType.TEXT_PLAIN_VALUE)
     public String isAlive() {
+        checkDatabase();
         return APPLICATION_LIVENESS;
     }
 
     @ResponseBody
     @RequestMapping(value = "/isReady", produces = MediaType.TEXT_PLAIN_VALUE)
     public String isReady() {
+        checkDatabase();
         return APPLICATION_READY;
+    }
+
+    private void checkDatabase(){
+        template.execute("select now();");
     }
 }
