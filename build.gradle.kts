53e708efad7425f6c4bdf7967fe12266ab683cbe
tuxbear <ole-andre.riga.johansen@nav.no>
2021-01-20 10:35:04 +0100

    Oppgraderer avhengigheter og fikser versjonsmismatch på jackson-scala-module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val springBootVersion = "2.2.6.RELEASE"
-val springVersion = "5.2.5.RELEASE"
-val springKafkaVersion = "2.4.5.RELEASE"
+val springBootVersion = "2.4.2"
+val springVersion = "5.3.3"
+val springKafkaVersion = "2.6.5"
 val micrometerVersion = "1.3.0"
 val flywayVersion = "6.1.4"
-val cxfVersion = "3.3.3"
-val clientMockVersion = "1.2.3-1.3.50-eap-5"
-val swaggerVersion = "2.7.0"
-val kotlinVersion = "1.3.72"
+val cxfVersion = "3.4.2"
+val swaggerVersion = "2.10.0"
+val kotlinVersion = "1.4.10"
 val hikariVersion = "3.4.2"
+val ktorVersion = "1.4.3"
 
 val mainClass = "no.nav.syfo.Application"
 
@@ -17,9 +17,9 @@ val githubPassword: String by project
 
 plugins {
     "maven-publish"
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.3.72"
-    id("org.jetbrains.kotlin.plugin.jpa") version "1.3.72"
+    id("org.jetbrains.kotlin.jvm") version "1.4.10"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.10"
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.4.10"
     id("org.flywaydb.flyway") version "5.1.4"
     id("org.sonarqube") version "3.0"
     java
@@ -56,7 +56,7 @@ tasks.withType<JacocoReport> {
 buildscript {
     dependencies {
         classpath("org.junit.platform:junit-platform-gradle-plugin:1.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-noarg:1.3.72")
+        classpath("org.jetbrains.kotlin:kotlin-noarg:1.4.10")
     }
 }
 
@@ -87,6 +87,16 @@ allOpen {
 
 
 dependencies {
+
+    // SNYK overrides
+    implementation("commons-collections:commons-collections:3.2.2")
+    // - end SNYK overrides
+    implementation("com.vladmihalcea:hibernate-types-52:2.10.2") {
+
+    }
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.0")
+
     // Spring
     implementation("io.springfox:springfox-swagger2:$swaggerVersion")
     implementation("io.springfox:springfox-swagger-ui:$swaggerVersion")
@@ -114,9 +124,8 @@ dependencies {
     implementation("jakarta.ws.rs:jakarta.ws.rs-api:2.1.6")
     implementation("com.sun.activation:javax.activation:1.2.0")
     implementation("com.sun.xml.messaging.saaj:saaj-impl:1.5.1")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.41")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.41")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.10")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
     implementation("org.apache.cxf:cxf-rt-ws-security:$cxfVersion")
     implementation("org.apache.cxf:cxf-rt-security:$cxfVersion")
     implementation("org.apache.cxf:cxf-rt-ws-policy:$cxfVersion")
@@ -127,7 +136,7 @@ dependencies {
     implementation("org.apache.cxf:cxf-rt-databinding-jaxb:$cxfVersion")
     runtimeOnly("org.apache.cxf:cxf-spring-boot-starter-jaxws:$cxfVersion")
     runtimeOnly("org.apache.cxf:cxf-rt-features-logging:$cxfVersion")
-    implementation("org.postgresql:postgresql:42.2.8")
+    implementation("org.postgresql:postgresql:42.2.13")
     implementation("org.apache.neethi:neethi:3.1.0")
     implementation("org.flywaydb:flyway-core:$flywayVersion")
 
@@ -147,35 +156,31 @@ dependencies {
     implementation("no.nav.syfo.sm:syfosm-common-networking:2019.09.03-10-50-64032e3b6381665e9f9c0914cef626331399e66d")
     implementation("no.nav:migrator:0.2.2")
     implementation("no.nav:vault-jdbc:1.3.1")
-    implementation("no.nav.common:log:2.2020.10.15_11.43-b1f02e7bd6ae")
-
+    implementation("no.nav.common:log:2.2021.01.05_08.07-2c586ccadf95")
 
     implementation("com.zaxxer:HikariCP:$hikariVersion")
 
     implementation("org.slf4j:slf4j-api:1.7.25")
-    implementation("net.logstash.logback:logstash-logback-encoder:4.10")
-    implementation("org.apache.httpcomponents:httpclient:4.5.6")
+    implementation("net.logstash.logback:logstash-logback-encoder:6.4")
+    implementation("org.apache.httpcomponents:httpclient:4.5.13")
     implementation("io.micrometer:micrometer-core:$micrometerVersion")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus:$micrometerVersion")
-    implementation("io.ktor:ktor-client-core:1.2.4")
-    implementation("io.ktor:ktor:1.2.4")
-    implementation("io.ktor:ktor-client-jackson:1.2.4")
-    implementation("io.ktor:ktor-client-apache:1.2.4")
-    testImplementation("org.springframework.kafka:spring-kafka-test:2.1.10.RELEASE")
-    testImplementation("junit:junit:4.12")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-jackson:$ktorVersion")
+    implementation("io.ktor:ktor-client-apache:$ktorVersion")
+    testImplementation("junit:junit:4.13.1")
     testImplementation("org.mockito:mockito-core:3.1.0")
     testImplementation("org.assertj:assertj-core:3.11.1")
     compileOnly("org.projectlombok:lombok:1.18.8")
     annotationProcessor("org.projectlombok:lombok:1.18.8")
     testCompileOnly("org.projectlombok:lombok:1.18.8")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.8")
-    implementation("com.google.guava:guava:28.1-jre")
-    testImplementation("io.ktor:ktor-client-mock:$clientMockVersion")
-    testImplementation("io.ktor:ktor-client-mock-jvm:$clientMockVersion")
+    implementation("com.google.guava:guava:30.0-jre")
+    testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
+    testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion")
     testImplementation("io.mockk:mockk:1.9.3")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
-    implementation("com.vladmihalcea:hibernate-types-52:2.9.3")
-    implementation("io.confluent:kafka-streams-avro-serde:5.0.0")
+    implementation("io.confluent:kafka-streams-avro-serde:5.2.1")
 }
 
 tasks.named<Jar>("jar") {
