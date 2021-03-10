773a8f144116dcd905130d4710c45778a8ae6a3a
sorvoja <trond.anders.sorvoja@nav.no>
2021-03-10 14:31:19 +0100

    WIP

---
+++ src/main/java/no/nav/syfo/util/DateUtil.kt
@@ -0,0 +1,27 @@
+package no.nav.syfo.util
+
+import no.nav.syfo.domain.Periode
+import java.time.LocalDate
+
+object DateUtil {
+    fun overlapperPerioder(a: Periode, b: Periode): Boolean {
+        return (compare(a.fom).isBetweenOrEqual(b)
+            || compare(a.tom).isBetweenOrEqual(b)
+            || compare(b.fom).isBetweenOrEqual(a)
+            || compare(b.tom).isBetweenOrEqual(a))
+    }
+
+    fun compare(localDate: LocalDate): LocalDateComparator {
+        return LocalDateComparator(localDate)
+    }
+
+    class LocalDateComparator internal constructor(private val localDate: LocalDate) {
+        fun isNotBetween(fom: LocalDate?, tom: LocalDate?): Boolean {
+            return localDate.isBefore(fom) || localDate.isAfter(tom)
+        }
+
+        fun isBetweenOrEqual(p: Periode): Boolean {
+            return !isNotBetween(p.fom, p.tom)
+        }
+    }
+}
