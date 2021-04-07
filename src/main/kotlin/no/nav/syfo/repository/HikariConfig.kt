a0830400edba56dc8d588d8af581a174d8588750
Ehsan Sadeghi <ehsan.sadeghi@nav.no>
2021-04-07 15:24:37 +0200

    brukt for db

---
+++ src/main/kotlin/no/nav/syfo/repository/HikariConfig.kt
@@ -0,0 +1,28 @@
+package no.nav.syfo.repository
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.metrics.prometheus.PrometheusMetricsTrackerFactory
+
+fun createHikariConfig(jdbcUrl: String, username: String? = null, password: String? = null, prometheusMetricsTrackerFactory: PrometheusMetricsTrackerFactory? = null) =
+        HikariConfig().apply {
+            this.jdbcUrl = jdbcUrl
+            maximumPoolSize = 3
+            minimumIdle = 1
+            idleTimeout = 10001
+            connectionTimeout = 2000
+            maxLifetime = 30001
+            driverClassName = "org.postgresql.Driver"
+            username?.let { this.username = it }
+            password?.let { this.password = it }
+            poolName = "defaultPool"
+            prometheusMetricsTrackerFactory?.let { metricsTrackerFactory = prometheusMetricsTrackerFactory }
+        }
+
+enum class Role {
+    admin, user, readonly;
+
+    override fun toString() = name.toLowerCase()
+}
+
+fun createTestHikariConfig() =
+        createHikariConfig("jdbc:postgresql://localhost:5432/fritakagp_db", "fritakagp", "fritakagp", null)
