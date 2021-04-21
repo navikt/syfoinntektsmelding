77cf56dfe578cb446e1d5a22b4a0d3a6c6e12636
Ehsan Sadeghi <ehsan.sadeghi@nav.no>
2021-04-21 15:59:30 +0200

    konvertert UtsattOppgaveServiceTest

--- src/test/kotlin/no/nav/syfo/syfoinnteksmelding/utsattoppgave/UtsattoppgaveServiceTest.kt
+++ src/test/kotlin/no/nav/syfo/syfoinnteksmelding/utsattoppgave/UtsattoppgaveServiceTest.kt
@@ -1,61 +1,32 @@
 package no.nav.syfo.syfoinnteksmelding.utsattoppgave
-/*
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import io.ktor.util.KtorExperimentalAPI
+import io.ktor.util.*
 import io.mockk.mockk
 import io.mockk.verify
-import no.nav.helse.arbeidsgiver.bakgrunnsjobb.BakgrunnsjobbRepository
 import no.nav.syfo.consumer.rest.OppgaveClient
 import no.nav.syfo.consumer.ws.BehandlendeEnhetConsumer
 import no.nav.syfo.dto.Tilstand
 import no.nav.syfo.dto.UtsattOppgaveEntitet
+import no.nav.syfo.utsattoppgave.UtsattOppgaveDAO
+import no.nav.syfo.utsattoppgave.UtsattOppgaveService
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.junit.runner.RunWith
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.TestPropertySource
-import org.springframework.test.context.junit4.SpringRunner
 import java.time.LocalDateTime
-import java.util.UUID
-import javax.transaction.Transactional
+import java.util.*
+
 
 @KtorExperimentalAPI
-@RunWith(SpringRunner::class)
-@ActiveProfiles("test")
-@Transactional
-@DataJpaTest
-@TestPropertySource(locations = ["classpath:application-test.properties"])
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@EnableAutoConfiguration(exclude = [(AutoConfigureTestDatabase::class)])
 open class UtsattOppgaveServiceTest {
 
     private var utsattOppgaveDAO: UtsattOppgaveDAO = mockk(relaxed = true)
     private var oppgaveClient: OppgaveClient = mockk()
     private var behandlendeEnhetConsumer: BehandlendeEnhetConsumer = mockk()
-    val bakgrunnsjobbRepomock = mockk<BakgrunnsjobbRepository>(relaxed = true)
     private lateinit var oppgaveService: UtsattOppgaveService
-    val objectMapper = ObjectMapper().registerModules(KotlinModule(), JavaTimeModule())
 
-    companion object {
-        @BeforeClass
-        @JvmStatic
-        fun beforeClass() {
-            System.setProperty("SECURITYTOKENSERVICE_URL", "joda")
-            System.setProperty("SRVSYFOINNTEKTSMELDING_USERNAME", "joda")
-            System.setProperty("SRVSYFOINNTEKTSMELDING_PASSWORD", "joda")
-        }
-    }
 
-    @Before
+    @BeforeEach
     fun setup() {
-        oppgaveService = UtsattOppgaveService(utsattOppgaveDAO, oppgaveClient, behandlendeEnhetConsumer,bakgrunnsjobbRepomock,objectMapper)
+        oppgaveService = UtsattOppgaveService(utsattOppgaveDAO, oppgaveClient, behandlendeEnhetConsumer,)
     }
 
     private val fnr = "fnr"
@@ -84,4 +55,4 @@ open class UtsattOppgaveServiceTest {
     )
 
 }
-*/
+
