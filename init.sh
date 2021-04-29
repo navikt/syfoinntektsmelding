e502b56f455d05bbcd6d40486883346703824535
tuxbear <ole-andre.riga.johansen@nav.no>
2021-04-29 14:09:10 +0200

    POH-808: Inject client secret kun om den finnes

--- init.sh
+++ init.sh
@@ -1,1 +1,4 @@
-export AAD_SYFOINNTEKTSMELDING_CLIENTID_PASSWORD=$(cat /var/run/secrets/nais.io/azuread/client_secret)
+CLIENT_SECRET_PATH=/var/run/secrets/nais.io/azuread/client_secret
+if test -f "$CLIENT_SECRET_PATH"; then
+  export AAD_SYFOINNTEKTSMELDING_CLIENTID_PASSWORD=$(cat $CLIENT_SECRET_PATH)
+fi
