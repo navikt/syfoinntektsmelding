b8d8388b6f066278a5b5b50f70c35f5ebb783e58
sorvoja <trond.anders.sorvoja@nav.no>
2021-05-07 12:27:49 +0200

    mangler i konfig, database migrering

--- src/main/resources/db/migration/V1_6__Bakgrunnsjobb.sql
+++ src/main/resources/db/migration/V1_6__Bakgrunnsjobb.sql
@@ -1,14 +1,13 @@
-create table bakgrunnsjobb
-(
-    jobb_id      uuid unique  not null,
-    type         VARCHAR(100) not null,
-    behandlet    timestamp,
-    opprettet    timestamp    not null,
+create table BAKGRUNNSJOBB (
+    JOBB_ID VARCHAR(100) unique not null,
+    TYPE VARCHAR(100) not null,
+    BEHANDLET timestamp,
+    OPPRETTET timestamp not null,
 
-    status       VARCHAR(50)  not null,
-    kjoeretid    timestamp    not null,
+    STATUS VARCHAR(50) not null,
+    KJOERETID timestamp not null,
 
-    forsoek      int          not null default 0,
-    maks_forsoek int          not null,
-    data         jsonb
-);
+    FORSOEK int not null default 0,
+    MAKS_FORSOEK int not null,
+    DATA jsonb
+)
