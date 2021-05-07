e3231bc0fc4710c8334b1e42dac5981f5abd3bc4
sorvoja <trond.anders.sorvoja@nav.no>
2021-05-07 10:04:56 +0200

    kopi av prod inn i prepod

--- src/main/kotlin/no/nav/syfo/koin/PreprodKoinProfile.kt
+++ src/main/kotlin/no/nav/syfo/koin/PreprodKoinProfile.kt
@@ -3,9 +3,41 @@ package no.nav.syfo.koin
 import io.ktor.config.*
 import io.ktor.util.*
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb.BakgrunnsjobbRepository
+import no.nav.helse.arbeidsgiver.bakgrunnsjobb.BakgrunnsjobbService
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb.PostgresBakgrunnsjobbRepository
+import no.nav.helse.arbeidsgiver.system.getString
 import no.nav.helse.arbeidsgiver.web.auth.AltinnAuthorizer
 import no.nav.helse.arbeidsgiver.web.auth.DefaultAltinnAuthorizer
+import no.nav.syfo.MetrikkVarsler
+import no.nav.syfo.behandling.InntektsmeldingBehandler
+import no.nav.syfo.consumer.SakConsumer
+import no.nav.syfo.consumer.azuread.AzureAdTokenConsumer
+import no.nav.syfo.consumer.rest.OppgaveClient
+import no.nav.syfo.consumer.rest.SakClient
+import no.nav.syfo.consumer.rest.TokenConsumer
+import no.nav.syfo.consumer.rest.aktor.AktorConsumer
+import no.nav.syfo.consumer.util.ws.LogErrorHandler
+import no.nav.syfo.consumer.util.ws.WsClient
+import no.nav.syfo.consumer.ws.*
+import no.nav.syfo.integration.kafka.*
+import no.nav.syfo.producer.InntektsmeldingProducer
+import no.nav.syfo.prosesser.FinnAlleUtgaandeOppgaverProcessor
+import no.nav.syfo.prosesser.FjernInntektsmeldingByBehandletProcessor
+import no.nav.syfo.repository.*
+import no.nav.syfo.service.EksisterendeSakService
+import no.nav.syfo.service.JournalpostService
+import no.nav.syfo.service.SaksbehandlingService
+import no.nav.syfo.util.Metrikk
+import no.nav.syfo.utsattoppgave.FeiletUtsattOppgaveMeldingProsessor
+import no.nav.syfo.utsattoppgave.UtsattOppgaveDAO
+import no.nav.syfo.utsattoppgave.UtsattOppgaveService
+import no.nav.tjeneste.virksomhet.arbeidsfordeling.v1.binding.ArbeidsfordelingV1
+import no.nav.tjeneste.virksomhet.behandleinngaaendejournal.v1.binding.BehandleInngaaendeJournalV1
+import no.nav.tjeneste.virksomhet.behandlesak.v2.BehandleSakV2
+import no.nav.tjeneste.virksomhet.inngaaendejournal.v1.binding.InngaaendeJournalV1
+import no.nav.tjeneste.virksomhet.journal.v2.binding.JournalV2
+import no.nav.tjeneste.virksomhet.oppgavebehandling.v3.binding.OppgavebehandlingV3
+import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
@@ -13,8 +45,153 @@ import org.koin.dsl.module
 fun preprodConfig(config: ApplicationConfig) = module {
     externalSystemClients(config)
 
-    single { PostgresBakgrunnsjobbRepository(get()) } bind BakgrunnsjobbRepository::class
 
-    single { DefaultAltinnAuthorizer(get()) } bind AltinnAuthorizer::class
+    single {
+        AktorConsumer(
+            get(),
+            config.getString("srvsyfoinntektsmelding.username"),
+            config.getString("aktoerregister_api_v1_url"),
+            get()
+        )
+    } bind AktorConsumer::class
+    single { TokenConsumer(get(), config.getString("security-token-service-token.url")) } bind TokenConsumer::class
+    single {
+        InntektsmeldingBehandler(
+            get(),
+            get(),
+            get(),
+            get(),
+            get(),
+            get(),
+            get()
+        )
+    } bind InntektsmeldingBehandler::class
+
+
+    single { InngaaendeJournalConsumer(get()) } bind InngaaendeJournalConsumer::class
+    single { BehandleInngaaendeJournalConsumer(get()) } bind BehandleInngaaendeJournalConsumer::class
+    single { JournalConsumer(get(), get()) } bind JournalConsumer::class
+    single { Metrikk() } bind Metrikk::class
+    single { BehandlendeEnhetConsumer(get(), get(), get()) } bind BehandlendeEnhetConsumer::class
+    single { JournalpostService(get(), get(), get(), get(), get()) } bind JournalpostService::class
+
+    single {
+        AzureAdTokenConsumer(
+            get(),
+            config.getString("aadaccesstoken_url"),
+            config.getString("aad_syfogsak_clientid_username"),
+            config.getString("aad_syfoinntektsmelding_clientid_password")
+        )
+    } bind AzureAdTokenConsumer::class
+
+    single {
+        SakConsumer(
+            get(),
+            get(),
+            config.getString("aad_syfoinntektsmelding_clientid_username"),
+            config.getString("sakconsumer_host_url")
+        )
+    } bind SakConsumer::class
+
+    single { EksisterendeSakService(get()) } bind EksisterendeSakService::class
+    single { InntektsmeldingService(InntektsmeldingRepositoryImp(get()), get()) } bind InntektsmeldingService::class
+    single { SakClient(config.getString("opprett_sak_url"), get()) } bind SakClient::class
+    single { SaksbehandlingService(get(), get(), get(), get()) } bind SaksbehandlingService::class
+
+    single {
+        JoarkHendelseKafkaClient(
+            joarkOnPremProperties(config).toMutableMap(),
+            config.getString("kafka_joark_hendelse_topic"), get(), get()
+        )
+    }
+    single { JoarkHendelseVarslingService(get()) }
+    single {
+        UtsattOppgaveKafkaClient(
+            utsattOppgaveOnPremProperties(config).toMutableMap(),
+            config.getString("kafka_utsatt_oppgave_topic"), get(), get(), get()
+        )
+    }
+    single { UtsattOppgaveVarslingService(get()) }
+
+
+    single {
+        InntektsmeldingProducer(
+            producerOnPremProperties(
+                config.getString("kafka_bootstrap_servers"),
+                config.getString("srvsyfoinntektsmelding.username"),
+                config.getString("srvsyfoinntektsmelding.password")
+            ), get()
+        )
+    } bind InntektsmeldingProducer::class
+
+    single { UtsattOppgaveDAO(UtsattOppgaveRepositoryImp(get())) }
+    single { OppgaveClient(config.getString("oppgavebehandling_url"), get(), get()) } bind OppgaveClient::class
+    single { UtsattOppgaveService(get(), get(), get()) } bind UtsattOppgaveService::class
+    single { FeiletUtsattOppgaveMeldingProsessor(get(), get()) }
+
+    single {
+        FjernInntektsmeldingByBehandletProcessor(
+            InntektsmeldingRepositoryImp(get()),
+            config.getString("lagringstidMåneder").toInt()
+        )
+    } bind FjernInntektsmeldingByBehandletProcessor::class
+    single { FinnAlleUtgaandeOppgaverProcessor(get(), get(), get()) } bind FinnAlleUtgaandeOppgaverProcessor::class
+
+    single { OppgavebehandlingConsumer(get()) } bind OppgavebehandlingConsumer::class
+
+    single { FeiletService(FeiletRepositoryImp(get())) } bind FeiletService::class
+
+    single { PostgresBakgrunnsjobbRepository(get()) } bind BakgrunnsjobbRepository::class
+    single { BakgrunnsjobbService(get(), bakgrunnsvarsler = MetrikkVarsler()) }
+
+    single {
+        WsClient<PersonV3>().createPort(
+            config.getString("virksomhet_person_3_endpointurl"),
+            PersonV3::class.java,
+            listOf(LogErrorHandler())
+        )
+    } bind PersonV3::class
+    single {
+        WsClient<ArbeidsfordelingV1>().createPort(
+            config.getString("virksomhet_arbeidsfordeling_v1_endpointurl"),
+            ArbeidsfordelingV1::class.java,
+            listOf(LogErrorHandler())
+        )
+    } bind ArbeidsfordelingV1::class
+    single {
+        WsClient<OppgavebehandlingV3>().createPort(
+            config.getString("servicegateway_url"),
+            OppgavebehandlingV3::class.java,
+            listOf(LogErrorHandler())
+        )
+    } bind OppgavebehandlingV3::class
+    single {
+        WsClient<JournalV2>().createPort(
+            config.getString("journal_v2_endpointurl"),
+            JournalV2::class.java,
+            listOf(LogErrorHandler())
+        )
+    } bind JournalV2::class
+    single {
+        WsClient<InngaaendeJournalV1>().createPort(
+            config.getString("inngaaendejournal_v1_endpointurl"),
+            InngaaendeJournalV1::class.java,
+            listOf(LogErrorHandler())
+        )
+    } bind InngaaendeJournalV1::class
+    single {
+        WsClient<BehandleSakV2>().createPort(
+            config.getString("virksomhet_behandlesak_v2_endpointurl"),
+            BehandleSakV2::class.java,
+            listOf(LogErrorHandler())
+        )
+    } bind BehandleSakV2::class
+    single {
+        WsClient<BehandleInngaaendeJournalV1>().createPort(
+            config.getString("behandleinngaaendejournal_v1_endpointurl"),
+            BehandleInngaaendeJournalV1::class.java,
+            listOf(LogErrorHandler())
+        )
+    } bind BehandleInngaaendeJournalV1::class
 }
 
