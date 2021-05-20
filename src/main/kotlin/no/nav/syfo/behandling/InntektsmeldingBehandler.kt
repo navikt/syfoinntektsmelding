d4fd9234f9336ff0b95522777aea2a9354186414
tuxbear <ole-andre.riga.johansen@nav.no>
2021-05-20 12:23:17 +0200

    Skrur oppgave timeout til 48 timer

--- src/main/kotlin/no/nav/syfo/behandling/InntektsmeldingBehandler.kt
+++ src/main/kotlin/no/nav/syfo/behandling/InntektsmeldingBehandler.kt
@@ -30,7 +30,7 @@ class InntektsmeldingBehandler(
 ) {
 
     private val consumerLocks = Striped.lock(8)
-    private val OPPRETT_OPPGAVE_FORSINKELSE = 12L;
+    private val OPPRETT_OPPGAVE_FORSINKELSE = 48L;
 
     fun behandle(arkivId: String, arkivreferanse: String): String? {
         val inntektsmelding = journalpostService.hentInntektsmelding(arkivId, arkivreferanse)
