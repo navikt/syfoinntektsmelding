c970603615f5bf62cf97a42ade0e43a769c9f6bc
Ehsan Sadeghi <ehsan.sadeghi@nav.no>
2021-05-21 08:42:42 +0200

    metriker hadde ikke de riktig tagene

--- src/main/kotlin/no/nav/syfo/util/Metrikk.kt
+++ src/main/kotlin/no/nav/syfo/util/Metrikk.kt
@@ -13,154 +13,130 @@ class Metrikk {
 
     val counters = HashMap<String, Counter>()
 
-    private fun proseseringsMetrikker(metricName: String, metricHelpText: String): Counter {
-        if (counters.containsKey(metricName)) {
-            return counters[metricName]!!
+    private fun proseseringsMetrikker(metricName: String, metricHelpText: String, labelNames: Array<String> ): Counter {
+        return if (counters.containsKey(metricName)) {
+            counters[metricName]!!
         } else {
             val counter = Counter.build()
                 .namespace(METRICS_NS)
                 .name(metricName)
-                .labelNames("hendelse")
+                .labelNames(*labelNames)
                 .help(metricHelpText)
                 .register()
 
             counters[metricName] = counter
-            return counter
+            counter
         }
     }
 
     fun tellInntektsmeldingerMottatt(inntektsmelding: Inntektsmelding) {
-        val harArbeidsforholdId = inntektsmelding.arbeidsforholdId != null
-        val metricHelpText = """
-                "harArbeidsforholdId", ${if (harArbeidsforholdId) "J" else "N"},
-                "arsakTilSending", ${inntektsmelding.arsakTilInnsending}
-        """.trimIndent()
-        proseseringsMetrikker("syfoinntektsmelding_inntektsmeldinger_mottatt", metricHelpText).labels("info").inc()
+        val labelNames = arrayOf("type", "info","harArbeidsforholdId",
+            if (inntektsmelding.arbeidsforholdId != null) "J" else "N",
+           "arsakTilSending",
+            inntektsmelding.arsakTilInnsending)
+
+        proseseringsMetrikker("syfoinntektsmelding_inntektsmeldinger_mottatt",
+            "Metrikker for mottatt inntektsmeldinger", labelNames).inc()
     }
 
-    fun tellInntektsmeldingerJournalfort() {
-        val metricHelpText = "tellInntektsmeldingerJournalfort"
-        proseseringsMetrikker("syfoinntektsmelding_inntektsmeldinger_journalfort", metricHelpText).labels("info").inc()
+    fun tellInntektsmeldingerJournalfort() = proseseringsMetrikker("syfoinntektsmelding_inntektsmeldinger_journalfort",
+            "Metrikker for journalfort inntektsmeldinger",arrayOf("type", "info")).inc()
 
-    }
 
-    fun tellOverlappendeInntektsmelding() {
-        val metricHelpText = """
-               "kobling", $OVERLAPPENDE
-        """.trimIndent()
-        proseseringsMetrikker("syfoinntektsmelding_inntektsmeldinger_kobling", metricHelpText).labels("info").inc()
-    }
+    fun tellOverlappendeInntektsmelding() = proseseringsMetrikker("syfoinntektsmelding_inntektsmeldinger_kobling",
+            "Metrikker for inntektsmeldinger kobling",arrayOf("type", "info", "kobling", OVERLAPPENDE)).inc()
 
-    fun tellInntektsmeldingSaksIdFraSyfo() {
-        val metricHelpText = """
-                "kobling", $SAK_FRA_SYFO
-        """.trimIndent()
-        proseseringsMetrikker( "syfoinntektsmelding_inntektsmeldinger_kobling", metricHelpText).labels("info").inc()
-    }
+    fun tellInntektsmeldingSaksIdFraSyfo() = proseseringsMetrikker( "syfoinntektsmelding_inntektsmeldinger_kobling", "Metrikker for inntektsmeldinger kobling",
+            arrayOf("type", "info",  "kobling", SAK_FRA_SYFO)).inc()
 
-    fun tellInntektsmeldingNySak() {
-        val metricHelpText = """
-                  "kobling", $NY_SAK
-        """.trimIndent()
-        proseseringsMetrikker( "syfoinntektsmelding_inntektsmeldinger_kobling", metricHelpText).labels("info").inc()
-    }
 
-    fun tellInntektsmeldingSykepengerUtland() {
-        val metricHelpText = ""
-        proseseringsMetrikker("syfoinntektsmelding_sykepenger_utland", metricHelpText).labels("info").inc()
-    }
+    fun tellInntektsmeldingNySak() =
+        proseseringsMetrikker( "syfoinntektsmelding_inntektsmeldinger_kobling",
+            "Metrikker for inntektsmeldinger kobling", arrayOf("type", "info",  "kobling", NY_SAK)).inc()
 
-    fun tellFeiletBakgrunnsjobb() {
-        val metricHelpText = ""
-        proseseringsMetrikker("syfoinntektsmelding_bakgrunnsjobb_feilet", metricHelpText).labels("info").inc()
-    }
 
-    fun tellStoppetBakgrunnsjobb() {
-        val metricHelpText = ""
-        proseseringsMetrikker("syfoinntektsmelding_bakgrunnsjobb_stoppet", metricHelpText).labels("info").inc()
-    }
+    fun tellInntektsmeldingSykepengerUtland() =
+        proseseringsMetrikker("syfoinntektsmelding_sykepenger_utland", "Metrikker for sykepenger utland", arrayOf("")).inc()
 
-    fun tellInntektsmeldingfeil() {
-        val metricHelpText = ""
-        proseseringsMetrikker("syfoinntektsmelding_inntektsmeldingfeil", metricHelpText).labels("error").inc()
-    }
 
-    fun tellBehandlingsfeil(feiltype: Feiltype) {
-        val metricHelpText = """
-                "feiltype", ${feiltype.navn}
-        """.trimIndent()
-        proseseringsMetrikker("syfoinntektsmelding_behandlingsfeil", metricHelpText).labels("error").inc()
-    }
+    fun tellFeiletBakgrunnsjobb() = proseseringsMetrikker("syfoinntektsmelding_bakgrunnsjobb_feilet",
+        "Metrikker for feilt bakgrunnjobb", arrayOf("")).inc()
 
-    fun tellIkkebehandlet() {
-        val metricHelpText = ""
-        proseseringsMetrikker("syfoinntektsmelding_ikkebehandlet", metricHelpText).labels("info").inc()
-    }
+    fun tellStoppetBakgrunnsjobb() = proseseringsMetrikker("syfoinntektsmelding_bakgrunnsjobb_stoppet",
+        "Metrikker for stoppet bakgrunnsjob", arrayOf("")).inc()
+
+
+    fun tellInntektsmeldingfeil() = proseseringsMetrikker("syfoinntektsmelding_inntektsmeldingfeil",
+        "Metrikker for inntektsmelding feiler", arrayOf("type", "error")).inc()
+
+
+    fun tellBehandlingsfeil(feiltype: Feiltype) = proseseringsMetrikker("syfoinntektsmelding_behandlingsfeil",
+        "Metrikker for behandlet feiler", arrayOf("feiltype", feiltype.navn)).inc()
+
+
+    fun tellIkkebehandlet() = proseseringsMetrikker("syfoinntektsmelding_ikkebehandlet",
+            "Metrikker for ikke behandlet", arrayOf("")).inc()
+
 
     fun tellJournalpoststatus(status: JournalStatus) {
         val metricHelpText = """
                 "status", ${status.name}
         """.trimIndent()
-        proseseringsMetrikker("syfoinntektsmelding_journalpost", metricHelpText).labels("info").inc()
+        proseseringsMetrikker("syfoinntektsmelding_journalpost",
+            "Metrikk for journal post status", arrayOf("type", "info", "status", status.name)).inc()
     }
 
-    fun tellInntektsmeldingLagtPåTopic() {
-        val metricHelpText = "tellInntektsmeldingLagtPåTopic"
-        proseseringsMetrikker("syfoinntektsmelding_inntektsmelding_lagt_pa_topic", metricHelpText).labels("info").inc()
-    }
+    fun tellInntektsmeldingLagtPåTopic() = proseseringsMetrikker("syfoinntektsmelding_inntektsmelding_lagt_pa_topic",
+        "Metrikker for InntektsmeldingLagt På topic", arrayOf("")).inc()
 
-    fun tellInntektsmeldingUtenArkivReferanse() {
-        val metricHelpText = "tellInntektsmeldingUtenArkivReferanse"
-        proseseringsMetrikker("syfoinntektsmelding_inntektsmelding_uten_arkivreferanse", metricHelpText).labels("info").inc()
-    }
+
+    fun tellInntektsmeldingUtenArkivReferanse() = proseseringsMetrikker("syfoinntektsmelding_inntektsmelding_uten_arkivreferanse",
+        "Metrikker for InntektsmeldingLagt uten arkiv refereanse", arrayOf("")).inc()
+
 
     fun tellInntektsmeldingerRedusertEllerIngenUtbetaling(begrunnelse: String?) {
-        val metricHelpText = """
-               "begrunnelse", $begrunnelse
-        """.trimIndent()
-        proseseringsMetrikker("syfoinntektsmelding_redusert_eller_ingen_utbetaling", metricHelpText).labels("info").inc()
+        proseseringsMetrikker("syfoinntektsmelding_redusert_eller_ingen_utbetaling",
+            "Metrikker for Inntektsmeldinger Redusert Eller Ingen Utbetaling", arrayOf("type", "info", "begrunnelse", begrunnelse!!)).inc()
     }
 
     fun tellArbeidsgiverperioder(antall: String?) {
-        val metricHelpText = """
-              "antall", $antall
-        """.trimIndent()
-        proseseringsMetrikker("syfoinntektsmelding_arbeidsgiverperioder", metricHelpText).labels("info").inc()
+        proseseringsMetrikker("syfoinntektsmelding_arbeidsgiverperioder",
+            "Metrikker for arbeidsgiverperioder", arrayOf("antall", antall!!)).inc()
     }
 
     fun tellKreverRefusjon(beløp: Int) {
         val metricHelpText = "tellKreverRefusjon"
         if (beløp <= 0) return
-        proseseringsMetrikker("syfoinntektsmelding_arbeidsgiver_krever_refusjon", metricHelpText).labels("info").inc()
-        proseseringsMetrikker("syfoinntektsmelding_arbeidsgiver_krever_refusjon_beloep", metricHelpText).labels("info")
-            .inc((beløp / 1000).toDouble()) // Teller beløpet i antall tusener for å unngå overflow
+        proseseringsMetrikker("syfoinntektsmelding_arbeidsgiver_krever_refusjon",
+            "Metrikker for KreverRefusjon", arrayOf("")).inc()
+        proseseringsMetrikker("syfoinntektsmelding_arbeidsgiver_krever_refusjon_beloep",
+            "Metrikker for KreverRefusjon", arrayOf("")).inc((beløp / 1000).toDouble()) // Teller beløpet i antall tusener for å unngå overflow
     }
 
-    fun tellNaturalytelse() {
-        val metricHelpText = "tellNaturalytelse"
-        proseseringsMetrikker("syfoinntektsmelding_faar_naturalytelse", metricHelpText).labels("info").inc()
-    }
+    fun tellNaturalytelse() = proseseringsMetrikker("syfoinntektsmelding_faar_naturalytelse",
+            "Metrikker for Naturalytelse", arrayOf("")).inc()
+
 
     fun tellOpprettOppgave(eksisterer: Boolean) {
-        val metricHelpText = """
-            "eksisterer", ${if (eksisterer) "J" else "N"}
-        """.trimIndent()
-        proseseringsMetrikker("syfoinntektsmelding_opprett_oppgave", metricHelpText).labels("info").inc()
+        proseseringsMetrikker("syfoinntektsmelding_opprett_oppgave",
+            "Metrikker for OpprettOppgave", arrayOf("eksisterer", if (eksisterer) "J" else "N")).inc()
     }
 
     fun tellOpprettFordelingsoppgave() {
-        val metricHelpText = "tellOpprettFordelingsoppgave"
-        proseseringsMetrikker("syfoinntektsmelding_opprett_fordelingsoppgave", metricHelpText).labels("info").inc()
+        proseseringsMetrikker("syfoinntektsmelding_opprett_fordelingsoppgave",
+            "Metrikker for Opprett Fordelingsoppgave", arrayOf("")).inc()
+
     }
 
     fun tellLagreFeiletMislykkes() {
-        val metricHelpText = "tellLagreFeiletMislykkes"
-        proseseringsMetrikker("syfoinntektsmelding_feilet_lagring_mislykkes", metricHelpText).labels("info").inc()
+        proseseringsMetrikker("syfoinntektsmelding_feilet_lagring_mislykkes",
+            "Metrikker for Lagre feilet mislykkes", arrayOf("")).inc()
     }
 
     fun tellRekjørerFeilet() {
         val metricHelpText = "tellRekjørerFeilet"
-        proseseringsMetrikker("syfoinntektsmelding_rekjorer", metricHelpText).labels("info").inc()
+        proseseringsMetrikker("syfoinntektsmelding_rekjorer",
+            "Metrikker for rekjører Feilet", arrayOf("")).inc()
     }
 
     companion object {
