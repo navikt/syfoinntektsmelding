212897e88483a8e4c39bed77c4cbe8fe8ca4812b
Morten Laukvik <laukvik@users.noreply.github.com>
2021-08-20 10:17:34 +0200

    SAF Clienter (#118)

---
+++ src/main/kotlin/no/nav/syfo/model/GraphQLResponse.kt
@@ -0,0 +1,23 @@
+package no.nav.syfo.graphql.model
+
+data class GraphQLResponse<T> (
+    val data: T,
+    val errors: List<ResponseError>?
+)
+
+data class ResponseError(
+    val message: String?,
+    val locations: List<ErrorLocation>?,
+    val path: List<String>?,
+    val extensions: ErrorExtension?
+)
+
+data class ErrorLocation(
+    val line: String?,
+    val column: String?
+)
+
+data class ErrorExtension(
+    val code: String?,
+    val classification: String?
+)
