954d8bc78a58547b266bc40b64c2956fe5ae307c
Morten Laukvik <laukvik@users.noreply.github.com>
2021-09-03 11:19:24 +0200

    Bruk felles måte å initiere httpclient for oppgaveclient (#134)

--- src/test/kotlin/no/nav/syfo/consumer/rest/MockHttpClient.kt
+++ src/test/kotlin/no/nav/syfo/consumer/rest/MockHttpClient.kt
@@ -1,8 +1,12 @@
 package no.nav.syfo.consumer.rest.dokarkiv
 
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.client.*
 import io.ktor.client.engine.mock.*
@@ -32,21 +36,25 @@ fun buildHttpClientText(status: HttpStatusCode, text: String = ""): HttpClient {
     }
 }
 
-fun buildHttpClientJson(status: HttpStatusCode, json: String = "{}"): HttpClient {
+fun buildHttpClientJson(status: HttpStatusCode, response: Any): HttpClient {
     return HttpClient(MockEngine) {
         install(JsonFeature) {
             serializer = JacksonSerializer {
-                registerKotlinModule()
+                registerModule(KotlinModule())
+                registerModule(Jdk8Module())
                 registerModule(JavaTimeModule())
-                configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                configure(SerializationFeature.INDENT_OUTPUT, true)
                 configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                accept(ContentType.Application.Json)
             }
             expectSuccess = false
         }
         engine {
             addHandler {
                 respond(
-                    json,
+                    jacksonObjectMapper().registerModule(JavaTimeModule()).writeValueAsString(response),
                     headers = headersOf("Content-Type" to listOf(ContentType.Application.Json.toString())),
                     status = status
                 )
