9dbb2d60ae99a88a98294713811eb3f276b37417
Morten Laukvik <morten.laukvik@nav.no>
2021-09-07 16:56:44 +0200

    SAF: Bruk Original format for henting inntektsmelding

--- src/main/kotlin/no/nav/syfo/client/saf/SafDokumentClient.kt
+++ src/main/kotlin/no/nav/syfo/client/saf/SafDokumentClient.kt
@@ -26,7 +26,7 @@ class SafDokumentClient constructor(
     ): ByteArray? {
         log.info("Henter dokument fra journalpostId $journalpostId, og dokumentInfoId $dokumentInfoId")
         val response = runBlocking {
-            httpClient.get<HttpStatement>("$url/hentdokument/$journalpostId/$dokumentInfoId/ARKIV") {
+            httpClient.get<HttpStatement>("$url/hentdokument/$journalpostId/$dokumentInfoId/ORIGINAL") {
                 accept(ContentType.Application.Xml)
                 header("Authorization", "Bearer ${stsClient.getToken()}")
                 header("Nav-Callid", MDCOperations.putToMDC(MDCOperations.MDC_CALL_ID, UUID.randomUUID().toString()))
