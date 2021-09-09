8e3e912e5b7d1938a0feee3636d710d4683b915f
Morten Laukvik <morten.laukvik@nav.no>
2021-09-09 13:53:56 +0200

    Legg tilbake API
    Sjekker om dette gjør at helsesjekkene virker igjen

---
+++ src/main/kotlin/no/nav/syfo/web/api/JournalInntektsmeldingRequest.kt
@@ -0,0 +1,6 @@
+package no.nav.syfo.web.api
+
+data class JournalInntektsmeldingRequest (
+    val arkivId: String,
+    val arkivReferanse: String
+)
--- src/test/kotlin/no/nav/syfo/slowtests/kafka/UtsattOppgaveKafkaConsumerTest.kt
+++ src/test/kotlin/no/nav/syfo/slowtests/kafka/UtsattOppgaveKafkaConsumerTest.kt
@@ -16,12 +16,11 @@ import no.nav.syfo.utsattOppgaveKakaData
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class UtsattOppgaveKafkaConsumerTest : SystemTestBase() {
     private lateinit var kafkaProdusent: KafkaAdminForTests
-    private val topicName = "topic"
+    private val topicName = "tbd.spre-oppgaver"
     private val objectMapper = ObjectMapper()
         .registerModule(KotlinModule())
         .registerModule(Jdk8Module())
