e8dc0b7c7336d7d7b3c5e6ae574d4835786a60e5
Gustav Berggren <gustav.berggren@nav.no>
2021-09-15 12:56:46 +0200

    POH-1041: Oppdater tester

--- src/test/kotlin/no/nav/syfo/slowtests/datapakke/DatapakkeJsonTest.kt
+++ src/test/kotlin/no/nav/syfo/slowtests/datapakke/DatapakkeJsonTest.kt
@@ -8,6 +8,7 @@ import no.nav.helse.arbeidsgiver.utils.loadFromResources
 import no.nav.security.mock.oauth2.http.json
 import no.nav.security.mock.oauth2.http.objectMapper
 import no.nav.syfo.slowtests.SystemTestBase
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
@@ -25,5 +26,16 @@ class DatapakkeJsonTest : SystemTestBase(){
         assertTrue(result.isObject)
         assertNotEquals(testData, result)
         assertEquals("\"Inntektsmelding sykepenger\"", result.get("title").toString())
+    }
+
+    @Test
+    @Disabled
+    fun `prøv å sende request`(){
+        runBlocking {
+            val response = httpClient.put<HttpResponse>("https://datakatalog-api.dev.intern.nav.no/v1/datapackage/fb74c8d14d9c579e05b0b4b587843e6b") {
+                contentType(ContentType.Application.Json)
+                body = objectMapper.readTree(testData)
+            }
+        }
     }
 }
