1e6391010d8f40188cb796749f04e8625f339bed
Trond Anders Sorvoja <trond.anders.sorvoja@nav.no>
2021-10-18 16:43:46 +0200

    Nytt endepunkt for login expiery (#173)

---
+++ src/main/kotlin/no/nav/syfo/web/auth/AuthUtils.kt
@@ -0,0 +1,22 @@
+package no.nav.syfo.web.auth
+
+import io.ktor.config.*
+import io.ktor.request.*
+import io.ktor.util.*
+import no.nav.security.token.support.core.jwt.JwtToken
+import java.time.Instant
+import java.util.*
+
+@KtorExperimentalAPI
+fun hentUtløpsdatoFraLoginToken(config: ApplicationConfig, request: ApplicationRequest): Date {
+    val tokenString = getTokenString(config, request)
+    return JwtToken(tokenString).jwtTokenClaims.expirationTime ?: Date.from(Instant.MIN)
+}
+
+private fun getTokenString(config: ApplicationConfig, request: ApplicationRequest): String {
+    val cookieName = config.configList("no.nav.security.jwt.issuers")[0].property("cookie_name").getString()
+
+    return request.cookies[cookieName]
+        ?: request.headers["Authorization"]?.replaceFirst("Bearer ", "")
+        ?: throw IllegalAccessException("Du må angi et identitetstoken som cookieen $cookieName eller i Authorization-headeren")
+}
