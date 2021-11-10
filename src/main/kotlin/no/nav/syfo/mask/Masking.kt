fcda2720a93a89d954a55faf98c72458995e0e99
Morten Laukvik <morten.laukvik@nav.no>
2021-11-10 14:57:17 +0100

    Fornying av build fila, snyk og deps bumping

--- src/main/kotlin/no/nav/syfo/mask/Masking.kt
+++ src/main/kotlin/no/nav/syfo/mask/Masking.kt
@@ -4,10 +4,9 @@ fun String.maskLast(count: Int): String {
     var x = this.length - count
     if (x < 0) {
         return this
-    } else if (x > this.length) {
+    }
+    if (x > this.length) {
         x = this.length
     }
-
     return this.substring(x, x + count).padStart(this.length, 'x')
 }
-
