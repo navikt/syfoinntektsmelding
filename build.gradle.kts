57b65ca719950fd73668f6b9037ba12e6e879640
Morten Laukvik <morten.laukvik@nav.no>
2021-11-10 14:45:37 +0100

    Fornying av build fila, snyk og deps bumping

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,27 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 val micrometerVersion = "1.6.3"
 val flywayVersion = "6.1.4"
 val cxfVersion = "3.4.4"
-val swaggerVersion = "2.10.0"
 val kotlinVersion = "1.4.10"
 val hikariVersion = "3.4.5"
 val ktorVersion = "1.5.3"
-val koinVersion = "2.0.1"
+val koinVersion = "3.1.2"
 val tokenSupportVersion = "1.3.1"
-val mockOAuth2ServerVersion = "0.3.4"
+val mockOAuth2ServerVersion = "0.2.1"
 val brukernotifikasjonSchemasVersion = "1.2021.01.18-11.12-b9c8c40b98d1"
 val jacksonVersion = "2.12.3"
 val junitJupiterVersion = "5.7.0"
 val assertJVersion = "3.12.2"
 val prometheusVersion = "0.6.0"
-
-val mainClass = "no.nav.syfo.AppKt"
-
 val githubPassword: String by project
 
 plugins {
-    application
-    kotlin("jvm") version "1.4.20"
+    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+    id("org.jetbrains.kotlin.jvm") version "1.4.20"
     id("com.github.ben-manes.versions") version "0.27.0"
     id("org.flywaydb.flyway") version "5.1.4"
-}
-
-application.mainClass.set(mainClass)
-
-buildscript {
-    dependencies {
-        classpath("org.junit.platform:junit-platform-gradle-plugin:1.2.0")
-    }
+    id("io.snyk.gradle.plugin.snykplugin") version "0.4"
+    // Apply the application plugin to add support for building a CLI application in Java.
+    application
 }
 
 repositories {
@@ -61,50 +50,18 @@ java {
 }
 
 dependencies {
-    constraints {
-        implementation("io.netty:netty-codec-http2") {
-            version {
-                strictly("4.1.68.Final")
-            }
-            because("snyk control")
-        }
-
-        implementation("io.netty:netty-transport-native-epoll") {
-            version {
-                strictly("4.1.59.Final")
-            }
-            because("snyk control")
-        }
-        testImplementation("org.eclipse.jetty:jetty-io") {
-            version {
-                strictly("11.0.2")
-            }
-            because("snyk control")
-        }
-        implementation("io.ktor:ktor-client-cio") {
-            version {
-                strictly("1.3.0")
-            }
-            because("snyk control")
-        }
-    }
-    implementation("io.netty:netty-codec:4.1.68.Final") // overstyrer transiente 4.1.44
-    implementation("io.netty:netty-codec-http:4.1.68.Final") // overstyrer transiente 4.1.51.Final gjennom ktor-server-netty
-    // SNYK overrides
-    implementation("commons-collections:commons-collections:3.2.2")
-    // - end SNYK overrides
-
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.google.guava:guava:30.1.1-jre")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.10")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
-
     implementation("org.apache.cxf:cxf-core:$cxfVersion")
     implementation("org.apache.cxf:cxf-rt-databinding-jaxb:$cxfVersion")
-
     implementation("org.postgresql:postgresql:42.2.23")
     implementation("org.apache.neethi:neethi:3.1.0")
     implementation("org.flywaydb:flyway-core:$flywayVersion")
-
-    // NAV
     implementation("no.nav.sykepenger.kontrakter:inntektsmelding-kontrakt:2020.04.06-12-19-94de1")
     implementation("no.nav.tjenestespesifikasjoner:nav-altinn-inntektsmelding:1.2021.02.22-10.45-4201aaea72fb")
     implementation("no.nav.syfo.sm:syfosm-common-rest-sts:2019.09.03-10-50-64032e3b6381665e9f9c0914cef626331399e66d")
@@ -115,24 +72,18 @@ dependencies {
     implementation("no.nav.security:token-client-core:$tokenSupportVersion")
     implementation("no.nav.security:token-validation-ktor:$tokenSupportVersion")
     implementation("no.nav.security:mock-oauth2-server:$mockOAuth2ServerVersion")
-
     implementation("com.zaxxer:HikariCP:$hikariVersion")
-
     implementation("org.slf4j:slf4j-api:1.7.25")
     implementation("net.logstash.logback:logstash-logback-encoder:6.4")
     implementation("org.apache.httpcomponents:httpclient:4.5.13")
     implementation("io.micrometer:micrometer-core:$micrometerVersion")
-    implementation("io.insert-koin:koin-core-jvm:3.1.2")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus:$micrometerVersion")
-
     implementation("com.google.guava:guava:30.0-jre")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
     implementation("io.confluent:kafka-streams-avro-serde:6.2.1")
     implementation("io.confluent:kafka-avro-serializer:6.2.1")
     implementation("org.apache.kafka:kafka-streams:2.8.0")
-
     testImplementation("io.mockk:mockk:1.11.0")
-
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-apache:$ktorVersion")
     testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
@@ -145,31 +96,27 @@ dependencies {
     implementation("io.ktor:ktor-jackson:$ktorVersion")
     implementation("io.ktor:ktor-locations:$ktorVersion")
     testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
-
-    implementation("io.insert-koin:koin-core:3.1.2")
-    implementation("io.insert-koin:koin-ktor:3.1.2")
-    testImplementation("io.insert-koin:koin-test:3.1.2")
-
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.0")
-
+    implementation("io.insert-koin:koin-core-jvm:$koinVersion")
+    implementation("io.insert-koin:koin-core:$koinVersion")
+    implementation("io.insert-koin:koin-ktor:$koinVersion")
+    testImplementation("io.insert-koin:koin-test:$koinVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:$jacksonVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
-
     testImplementation("org.assertj:assertj-core:$assertJVersion")
     testImplementation(platform("org.junit:junit-bom:$junitJupiterVersion"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-
     implementation("io.prometheus:simpleclient_common:$prometheusVersion")
     implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
     implementation("no.nav.tjenestespesifikasjoner:altinn-correspondence-agency-external-basic:1.2019.09.25-00.21-49b69f0625e0")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
 }
 
-tasks.named<Jar>("jar") {
+tasks.jar {
     archiveBaseName.set("app")
 
     manifest {
-        attributes["Main-Class"] = mainClass
+        attributes["Main-Class"] = application.mainClass
         attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
             it.name
         }
@@ -184,14 +131,18 @@ tasks.named<Jar>("jar") {
     }
 }
 
-tasks.named<KotlinCompile>("compileKotlin") {
-    kotlinOptions.jvmTarget = "11"
-    kotlinOptions.suppressWarnings = true
+tasks.compileKotlin {
+    kotlinOptions {
+        jvmTarget = "11"
+        suppressWarnings = true
+    }
 }
 
-tasks.named<KotlinCompile>("compileTestKotlin") {
-    kotlinOptions.jvmTarget = "11"
-    kotlinOptions.suppressWarnings = true
+tasks.compileTestKotlin {
+    kotlinOptions {
+        jvmTarget = "11"
+        suppressWarnings = true
+    }
 }
 
 tasks.withType<Test> {
@@ -212,4 +163,11 @@ task<Test>("slowTests") {
     include("no/nav/syfo/slowtests/**")
     outputs.upToDateWhen { false }
     group = "verification"
+}
+
+configure<io.snyk.gradle.plugin.SnykExtension> {
+    setSeverity("high")
+    setAutoDownload(true)
+    setAutoUpdate(true)
+    setArguments("--all-sub-projects")
 }
