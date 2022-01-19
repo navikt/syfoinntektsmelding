08623b7674408adb96dd8ba9153dffa4d5983837
Gustav Berggren <gustav.berggren@nav.no>
2022-01-19 16:18:59 +0100

    POH-1169: Oppgrader felles backend

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
     implementation("no.nav.syfo.sm:syfosm-common-networking:2019.09.25-05-44-08e26429f4e37cd57d99ba4d39fc74099a078b97")
     implementation("no.nav:vault-jdbc:1.3.7")
     implementation("no.nav.common:log:2.2021.09.17_07.09-67428a6422cc")
-    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:2021.06.28-09-42-e08ae")
+    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:2022.01.18-08-47-f6aa0")
     implementation("no.nav.security:token-client-core:$tokenSupportVersion")
     implementation("no.nav.security:token-validation-ktor:$tokenSupportVersion")
     implementation("no.nav.security:mock-oauth2-server:$mockOAuth2ServerVersion")
