41166e169062d0abe3452566910db1c82c7094b3
Morten Laukvik <morten.laukvik@nav.no>
2022-01-27 15:33:52 +0100

    Fjern ubrukt JAXB kode

--- src/main/kotlin/no/nav/syfo/util/JAXB.kt
+++ src/main/kotlin/no/nav/syfo/util/JAXB.kt
@@ -1,11 +1,8 @@
 package no.nav.syfo.util
 import no.nav.melding.virksomhet.dokumentnotifikasjon.v1.ObjectFactory
 import java.io.StringReader
-import java.io.StringWriter
 import javax.xml.bind.JAXBContext
 import javax.xml.bind.JAXBException
-import javax.xml.bind.Marshaller
-import javax.xml.transform.stream.StreamResult
 
 object JAXB {
     private var FORSENDELSESINFORMASJON: JAXBContext? = null
@@ -20,28 +17,6 @@ object JAXB {
                 no.seres.xsd.nav.inntektsmelding_m._20180924.ObjectFactory::class.java,
                 no.seres.xsd.nav.inntektsmelding_m._20181211.ObjectFactory::class.java
             )
-        } catch (e: JAXBException) {
-            throw RuntimeException(e)
-        }
-    }
-
-    fun marshallForsendelsesinformasjon(element: Any?): String {
-        return try {
-            val writer = StringWriter()
-            val marshaller = FORSENDELSESINFORMASJON!!.createMarshaller()
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true)
-            marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8")
-            marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true)
-            marshaller.marshal(element, StreamResult(writer))
-            writer.toString()
-        } catch (e: JAXBException) {
-            throw RuntimeException(e)
-        }
-    }
-
-    fun <T> unmarshalForsendelsesinformasjon(melding: String?): T {
-        return try {
-            FORSENDELSESINFORMASJON!!.createUnmarshaller().unmarshal(StringReader(melding)) as T
         } catch (e: JAXBException) {
             throw RuntimeException(e)
         }
