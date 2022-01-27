4860774e6076c1800d4da8e0c887f96010b7cb9e
Morten Laukvik <morten.laukvik@nav.no>
2022-01-27 15:25:55 +0100

    Bruk uppercase og ikke toUpperCase

--- src/main/kotlin/no/nav/syfo/mapping/FellesMapper.kt
+++ src/main/kotlin/no/nav/syfo/mapping/FellesMapper.kt
@@ -1,16 +1,11 @@
 package no.nav.syfo.consumer.ws.mapping
 
 import no.nav.syfo.domain.inntektsmelding.Naturalytelse
-import java.time.LocalDateTime
+import java.util.Locale
 import javax.xml.bind.JAXBElement
-import javax.xml.datatype.XMLGregorianCalendar
 
 fun mapNaturalytelseType(naturalytelseType: JAXBElement<String>) =
     naturalytelseType?.value?.let { n ->
-        if (Naturalytelse.values().map { it.name }.contains(n.toUpperCase())) Naturalytelse.valueOf(n.toUpperCase()) else Naturalytelse.ANNET
+        if (Naturalytelse.values().map { it.name }.contains(n.uppercase(Locale.getDefault()))) Naturalytelse.valueOf(n.uppercase(Locale.getDefault())) else Naturalytelse.ANNET
     }
         ?: Naturalytelse.ANNET
-
-fun mapXmlGregorianTilLocalDate(xmlGreg: XMLGregorianCalendar): LocalDateTime {
-    return xmlGreg.toGregorianCalendar().toZonedDateTime().toLocalDateTime()
-}
