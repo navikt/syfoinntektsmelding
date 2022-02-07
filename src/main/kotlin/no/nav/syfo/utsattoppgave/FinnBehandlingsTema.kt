1da8daa0846e09e97fb7dda8ac4e7b093353c430
Morten Laukvik <morten.laukvik@nav.no>
2022-02-07 10:10:19 +0100

    syfoinntektsmelding-245 Lage annen type oppgave i Spinn dersom arbeidsgiver ikke utbetaler lønn

--- src/main/kotlin/no/nav/syfo/utsattoppgave/FinnBehandlingsTema.kt
+++ src/main/kotlin/no/nav/syfo/utsattoppgave/FinnBehandlingsTema.kt
@@ -3,25 +3,33 @@ package no.nav.syfo.utsattoppgave
 import no.nav.syfo.domain.inntektsmelding.Inntektsmelding
 import java.math.BigDecimal
 
-fun finnBehandlingsTema(inntektsmelding: Inntektsmelding): String {
+enum class BehandlingsTema(var navn: String) {
+    IKKE_REFUSJON("1"),
+    REFUSJON_UTEN_DATO("0"),
+    REFUSJON_MED_DATO("2"),
+    REFUSJON_LITEN_LØNN("3")
+}
+
+fun finnBehandlingsTema(inntektsmelding: Inntektsmelding): BehandlingsTema {
     val refusjon = inntektsmelding.refusjon
     if (refusjon.beloepPrMnd == null || refusjon.beloepPrMnd < BigDecimal(1)) {
         // 1). Ikke refusjon
-        return "1"
+        return BehandlingsTema.IKKE_REFUSJON
     } else {
         // Krever refusjon
         if (refusjon.opphoersdato == null) {
             // Uten dato
             if (refusjon.beloepPrMnd < inntektsmelding.beregnetInntekt) {
                 // 3). Refusjon mindre enn månedslønn
-                return "3"
+                return BehandlingsTema.REFUSJON_LITEN_LØNN
             } else {
                 // 0). Normal
-                return "0"
+                return BehandlingsTema.REFUSJON_UTEN_DATO
             }
         } else {
             // 2). Med dato
-            return "2"
+            return BehandlingsTema.REFUSJON_MED_DATO
         }
     }
 }
+
--- src/test/kotlin/no/nav/syfo/utsattoppgave/FinnBehandlingsTemaKtTest.kt
+++ src/test/kotlin/no/nav/syfo/utsattoppgave/FinnBehandlingsTemaKtTest.kt
@@ -17,37 +17,31 @@ internal class FinnBehandlingsTemaKtTest {
     @Test
     fun krever_ikke_refusjon_ved_null_beløp() {
         var inntektsmelding = mockInntektsmelding(Refusjon(beloepPrMnd = null, LocalDate.now()), BigDecimal(11000))
-        assertEquals("1", finnBehandlingsTema(inntektsmelding))
+        assertEquals(BehandlingsTema.IKKE_REFUSJON, finnBehandlingsTema(inntektsmelding))
     }
 
     @Test
     fun krever_ikke_refusjon_ved_tomt_beløp() {
         var inntektsmelding = mockInntektsmelding(Refusjon(beloepPrMnd = BigDecimal(0.2), LocalDate.now()), BigDecimal(11000))
-        assertEquals("1", finnBehandlingsTema(inntektsmelding))
+        assertEquals(BehandlingsTema.IKKE_REFUSJON, finnBehandlingsTema(inntektsmelding))
     }
 
     @Test
     fun krever_refusjon_med_dato() {
         val inntektsmelding = mockInntektsmelding(Refusjon(beloepPrMnd = BigDecimal(14000), LocalDate.now()), BigDecimal(11000))
-        assertEquals("2", finnBehandlingsTema(inntektsmelding))
+        assertEquals(BehandlingsTema.REFUSJON_MED_DATO, finnBehandlingsTema(inntektsmelding))
     }
 
     @Test
     fun krever_refusjon_mindre_månedslønn() {
-        var inntektsmelding = mockInntektsmelding(Refusjon(beloepPrMnd = BigDecimal(17000), LocalDate.now()), BigDecimal(11000))
-        assertEquals("3", finnBehandlingsTema(inntektsmelding))
+        var inntektsmelding = mockInntektsmelding(Refusjon(beloepPrMnd = BigDecimal(10000), null), BigDecimal(11000))
+        assertEquals(BehandlingsTema.REFUSJON_LITEN_LØNN, finnBehandlingsTema(inntektsmelding))
     }
 
     @Test
     fun vanlig_flyt_normal_lønn() {
-        var inntektsmelding = mockInntektsmelding(Refusjon(beloepPrMnd = BigDecimal(17000), LocalDate.now()), BigDecimal(17000))
-        assertEquals("0", finnBehandlingsTema(inntektsmelding))
-    }
-
-    @Test
-    fun vanlig_flyt() {
-        val inntektsmelding = mockInntektsmelding(Refusjon(beloepPrMnd = BigDecimal(13000), LocalDate.now()), BigDecimal(11000))
-        assertEquals("0", finnBehandlingsTema(inntektsmelding))
+        var inntektsmelding = mockInntektsmelding(Refusjon(beloepPrMnd = BigDecimal(17000), null), BigDecimal(17000))
+        assertEquals(BehandlingsTema.REFUSJON_UTEN_DATO, finnBehandlingsTema(inntektsmelding))
     }
 
     fun mockInntektsmelding(refusjon: Refusjon, inntekt: BigDecimal): Inntektsmelding {
@@ -64,14 +58,16 @@ internal class FinnBehandlingsTemaKtTest {
             arbeidsgiverperioder = emptyList(),
             beregnetInntekt = inntekt,
             refusjon = refusjon,
-            endringerIRefusjon = emptyList(), opphørAvNaturalYtelse = emptyList(),
+            endringerIRefusjon = emptyList(),
+            opphørAvNaturalYtelse = emptyList(),
             gjenopptakelserNaturalYtelse = emptyList(),
             gyldighetsStatus = Gyldighetsstatus.GYLDIG,
             arkivRefereranse = "ar123",
             feriePerioder = emptyList(),
             førsteFraværsdag = LocalDate.now(),
             mottattDato = LocalDateTime.now(),
-            sakId = "sak123", aktorId = "aktor123",
+            sakId = "sak123",
+            aktorId = "aktor123",
             begrunnelseRedusert = "grunn",
             avsenderSystem = AvsenderSystem("", ""),
             nærRelasjon = false,
