5e5404e5ea06e22e993919d6fe98a56a516e20c9
Morten Laukvik <laukvik@users.noreply.github.com>
2022-03-23 11:07:05 +0100

    Bump felles backend til 2022.03.23-10-47-91ba1 (#294)

--- src/main/kotlin/no/nav/syfo/koin/LocalKoinProfile.kt
+++ src/main/kotlin/no/nav/syfo/koin/LocalKoinProfile.kt
@@ -2,6 +2,7 @@ package no.nav.syfo.koin
 
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.config.ApplicationConfig
+import io.ktor.util.KtorExperimentalAPI
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb.BakgrunnsjobbRepository
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb.BakgrunnsjobbService
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb.PostgresBakgrunnsjobbRepository
@@ -55,6 +56,7 @@ import org.koin.dsl.bind
 import org.koin.dsl.module
 import javax.sql.DataSource
 
+@OptIn(KtorExperimentalAPI::class)
 fun localDevConfig(config: ApplicationConfig) = module {
     mockExternalDependecies()
 
