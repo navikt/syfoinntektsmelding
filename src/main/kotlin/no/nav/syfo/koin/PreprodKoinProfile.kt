46019c000e7519d631d9a5e9ecf0c03654f5ab79
Morten Laukvik <morten.laukvik@nav.no>
2022-03-23 11:00:26 +0100

    Bump felles backend til 2022.03.23-10-47-91ba1

--- src/main/kotlin/no/nav/syfo/koin/PreprodKoinProfile.kt
+++ src/main/kotlin/no/nav/syfo/koin/PreprodKoinProfile.kt
@@ -2,6 +2,7 @@ package no.nav.syfo.koin
 
 import com.zaxxer.hikari.HikariConfig
 import io.ktor.config.ApplicationConfig
+import io.ktor.util.KtorExperimentalAPI
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb.BakgrunnsjobbRepository
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb.BakgrunnsjobbService
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb.PostgresBakgrunnsjobbRepository
@@ -56,6 +57,7 @@ import org.koin.dsl.bind
 import org.koin.dsl.module
 import javax.sql.DataSource
 
+@OptIn(KtorExperimentalAPI::class)
 fun preprodConfig(config: ApplicationConfig) = module {
     externalSystemClients(config)
     single {
