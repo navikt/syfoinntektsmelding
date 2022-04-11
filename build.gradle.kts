3b01716719156bcf9a99839ef8c8d35898065cf5
Morten Laukvik <morten.laukvik@nav.no>
2022-04-11 14:48:20 +0200

    Test suites

--- build.gradle.kts
+++ build.gradle.kts
@@ -179,3 +179,16 @@ testing {
         }
     }
 }
+
+tasks.koverVerify {
+    includes = listOf("no.nav.syfo.*")            // inclusion rules for classes
+    //excludes = listOf("com.example.subpackage.*") // exclusion rules for classes
+
+    rule {
+        name = "Minimal line coverage rate in percent"
+        bound {
+            minValue = 75
+        }
+    }
+}
+
