b8a41cd4db13d5d8cde406983a7dc635cfc180c5
Morten Laukvik <morten.laukvik@nav.no>
2022-05-05 09:25:56 +0200

    Logging

--- src/main/kotlin/no/nav/syfo/domain/inntektsmelding/Inntektsmelding.kt
+++ src/main/kotlin/no/nav/syfo/domain/inntektsmelding/Inntektsmelding.kt
@@ -2,6 +2,8 @@ package no.nav.syfo.domain.inntektsmelding
 
 import no.nav.syfo.domain.JournalStatus
 import no.nav.syfo.domain.Periode
+import no.nav.syfo.service.InntektsmeldingService
+import org.slf4j.LoggerFactory
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -38,6 +40,8 @@ data class Inntektsmelding(
     val årsakEndring: String? = null,
 ) {
     fun isDuplicate(inntektsmelding: Inntektsmelding): Boolean {
+        val log = LoggerFactory.getLogger(Inntektsmelding::class.java)
+        log.info("isDuplicat: ${this.toString()} vs ${inntektsmelding.toString()}")
         return this.equals(
             inntektsmelding.copy(
                 id = this.id,
