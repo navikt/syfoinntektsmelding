3dfdb6da2e229a9037aa9c974cc8e9aa034b94b0
Mikael Bjerga <mikael.bjerga@nav.no>
2022-06-30 12:21:54 +0200

    Forenkle funksjon for opprettelse av en logger, og fiks pakkenavn

--- src/main/kotlin/no/nav/syfo/service/BehandlendeEnhetConsumer.kt
+++ src/main/kotlin/no/nav/syfo/service/BehandlendeEnhetConsumer.kt
@@ -1,7 +1,6 @@
 package no.nav.syfo.service
 
 import kotlinx.coroutines.runBlocking
-import log
 import no.nav.helse.arbeidsgiver.integrasjoner.pdl.PdlClient
 import no.nav.syfo.behandling.BehandlendeEnhetFeiletException
 import no.nav.syfo.behandling.IngenAktivEnhetException
@@ -11,6 +10,7 @@ import no.nav.syfo.client.norg.Norg2Client
 import no.nav.syfo.domain.GeografiskTilknytningData
 import no.nav.syfo.util.MDCOperations
 import no.nav.syfo.util.Metrikk
+import no.nav.syfo.util.logger
 
 const val SYKEPENGER_UTLAND = "4474"
 const val SYKEPENGER = "SYK"
@@ -20,8 +20,7 @@ class BehandlendeEnhetConsumer(
     private val norg2Client: Norg2Client,
     private val metrikk: Metrikk
 ) {
-
-    var log = log()
+    private val logger = this.logger()
 
     fun hentBehandlendeEnhet(fnr: String, uuid: String): String {
         val geografiskTilknytning = hentGeografiskTilknytning(fnr)
@@ -38,7 +37,7 @@ class BehandlendeEnhetConsumer(
             val arbeidsfordelinger = runBlocking {
                 norg2Client.hentAlleArbeidsfordelinger(criteria, callId)
             }
-            log.info("Fant enheter: " + arbeidsfordelinger.toString())
+            logger.info("Fant enheter: " + arbeidsfordelinger.toString())
             val behandlendeEnhet = finnAktivBehandlendeEnhet(
                 arbeidsfordelinger,
                 geografiskTilknytning.geografiskTilknytning
@@ -46,10 +45,10 @@ class BehandlendeEnhetConsumer(
             if (SYKEPENGER_UTLAND == behandlendeEnhet) {
                 metrikk.tellInntektsmeldingSykepengerUtland()
             }
-            log.info("Fant geografiskTilknytning ${geografiskTilknytning.geografiskTilknytning} med behandlendeEnhet $behandlendeEnhet for inntektsmelding $uuid")
+            logger.info("Fant geografiskTilknytning ${geografiskTilknytning.geografiskTilknytning} med behandlendeEnhet $behandlendeEnhet for inntektsmelding $uuid")
             return behandlendeEnhet
         } catch (e: RuntimeException) {
-            log.error("Klarte ikke å hente behandlende enhet!", e)
+            logger.error("Klarte ikke å hente behandlende enhet!", e)
             throw BehandlendeEnhetFeiletException(e)
         }
     }
