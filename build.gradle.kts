1b976b31d6bfef7f5b7e8e7831b500406b812969
Morten Laukvik <morten.laukvik@nav.no>
2022-07-07 12:55:04 +0200

    Bump jvm 1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ val annotationApiVersion: String by project
 val altinnCorrespondanceVersion: String by project
 
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
     id("com.github.ben-manes.versions") version "0.42.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
     id("org.flywaydb.flyway") version "8.4.2"
