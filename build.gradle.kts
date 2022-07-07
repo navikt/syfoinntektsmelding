2d3b3e5ea39d7f20da9c6688406f4f4cdcfc27e5
Morten Laukvik <morten.laukvik@nav.no>
2022-07-07 11:03:31 +0200

    Bruk variabler for alle versjoner

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val version = "1.0"
 val mainClass = "no.nav.syfo.AppKt"
 val group = "no.nav.syfo"
+val githubPassword: String by project
 // Dependencies
 val micrometerVersion = "1.8.3"
 val flywayVersion = "8.5.10"
@@ -19,7 +20,26 @@ val junitJupiterVersion = "5.8.2"
 val assertJVersion = "3.23.1"
 val prometheusVersion = "0.15.0"
 val joarkHendelseVersion = "96ec5ebb"
-val githubPassword: String by project
+val mockkVersion = "1.12.2"
+val jetbrainsStdLib = "kotlin-stdlib-jdk8"
+val guavaVersion = "31.0.1-jre"
+val kotlinLibsVersion = "1.7.0"
+val postgresVersion = "42.3.1"
+val neethiVersion = "3.2.0"
+val imkontraktVersion = "2022.02.25-10-37-3934b"
+val altinnInntektsmeldingVersion = "1.2021.02.22-10.45-4201aaea72fb"
+val navSyfoCommonVersion = "2019.09.25-05-44-08e26429f4e37cd57d99ba4d39fc74099a078b97"
+val vaultJdbcVersion = "1.3.7"
+val navLogVersion = "2.2022.02.18_14.38-8d8bb494bd41"
+val fellesBackendVersion = "2022.07.06-11-34-2e18c"
+val slf4Version = "1.7.36"
+val logbackVersion = "7.2"
+val apacheHttpClientVersion = "4.5.13"
+val kotlinCoroutinesVersion = "1.6.0-native-mt"
+val kafkaVersion = "7.1.1"
+val apacheKafkaStreamsVersion = "7.1.1-ce"
+val annotationApiVersion = "1.3.2"
+val altinnCorrespondanceVersion = "1.2019.09.25-00.21-49b69f0625e0"
 
 plugins {
     kotlin("jvm") version "1.7.0"
@@ -123,42 +143,41 @@ dependencies {
     implementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     implementation("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
     testImplementation("io.insert-koin:koin-test:$koinVersion")
-    testImplementation("io.mockk:mockk:1.12.2")
+    testImplementation("io.mockk:mockk:$mockkVersion")
     testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
     testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion")
     testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
     testImplementation("org.assertj:assertj-core:$assertJVersion")
     implementation("no.nav.teamdokumenthandtering:teamdokumenthandtering-avro-schemas:$joarkHendelseVersion")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.google.guava:guava:31.0.1-jre")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.0")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.0")
+    implementation("org.jetbrains.kotlin:$jetbrainsStdLib")
+    implementation("com.google.guava:guava:$guavaVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinLibsVersion")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinLibsVersion")
     implementation("org.apache.cxf:cxf-core:$cxfVersion")
     implementation("org.apache.cxf:cxf-rt-databinding-jaxb:$cxfVersion")
-    implementation("org.postgresql:postgresql:42.3.1")
-    implementation("org.apache.neethi:neethi:3.2.0")
+    implementation("org.postgresql:postgresql:$postgresVersion")
+    implementation("org.apache.neethi:neethi:$neethiVersion")
     implementation("org.flywaydb:flyway-core:$flywayVersion")
-    implementation("no.nav.sykepenger.kontrakter:inntektsmelding-kontrakt:2022.02.25-10-37-3934b")
-    implementation("no.nav.tjenestespesifikasjoner:nav-altinn-inntektsmelding:1.2021.02.22-10.45-4201aaea72fb")
-    implementation("no.nav.syfo.sm:syfosm-common-rest-sts:2019.09.25-05-44-08e26429f4e37cd57d99ba4d39fc74099a078b97")
-    implementation("no.nav.syfo.sm:syfosm-common-networking:2019.09.25-05-44-08e26429f4e37cd57d99ba4d39fc74099a078b97")
-    implementation("no.nav:vault-jdbc:1.3.7")
-    implementation("no.nav.common:log:2.2022.02.18_14.38-8d8bb494bd41")
-    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:2022.07.06-11-34-2e18c")
+    implementation("no.nav.sykepenger.kontrakter:inntektsmelding-kontrakt:$imkontraktVersion")
+    implementation("no.nav.tjenestespesifikasjoner:nav-altinn-inntektsmelding:$altinnInntektsmeldingVersion")
+    implementation("no.nav.syfo.sm:syfosm-common-rest-sts:$navSyfoCommonVersion")
+    implementation("no.nav.syfo.sm:syfosm-common-networking:$navSyfoCommonVersion")
+    implementation("no.nav:vault-jdbc:$vaultJdbcVersion")
+    implementation("no.nav.common:log:$navLogVersion")
+    implementation("no.nav.helsearbeidsgiver:helse-arbeidsgiver-felles-backend:$fellesBackendVersion")
     implementation("no.nav.security:token-client-core:$tokenSupportVersion")
     implementation("no.nav.security:token-validation-ktor:$tokenSupportVersion")
     implementation("no.nav.security:mock-oauth2-server:$mockOAuth2ServerVersion")
     implementation("com.zaxxer:HikariCP:$hikariVersion")
-    implementation("org.slf4j:slf4j-api:1.7.36")
-    implementation("net.logstash.logback:logstash-logback-encoder:7.2")
-    implementation("org.apache.httpcomponents:httpclient:4.5.13")
+    implementation("org.slf4j:slf4j-api:$slf4Version")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logbackVersion")
+    implementation("org.apache.httpcomponents:httpclient:$apacheHttpClientVersion")
     implementation("io.micrometer:micrometer-core:$micrometerVersion")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus:$micrometerVersion")
-    implementation("com.google.guava:guava:31.0.1-jre")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-native-mt")
-    implementation("io.confluent:kafka-streams-avro-serde:7.1.1")
-    implementation("io.confluent:kafka-avro-serializer:7.1.1")
-    implementation("org.apache.kafka:kafka-streams:7.1.1-ce")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
+    implementation("io.confluent:kafka-streams-avro-serde:$kafkaVersion")
+    implementation("io.confluent:kafka-avro-serializer:$kafkaVersion")
+    implementation("org.apache.kafka:kafka-streams:$apacheKafkaStreamsVersion")
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-apache:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
@@ -176,6 +195,6 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
     implementation("io.prometheus:simpleclient_common:$prometheusVersion")
     implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
-    implementation("no.nav.tjenestespesifikasjoner:altinn-correspondence-agency-external-basic:1.2019.09.25-00.21-49b69f0625e0")
-    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("no.nav.tjenestespesifikasjoner:altinn-correspondence-agency-external-basic:$altinnCorrespondanceVersion")
+    implementation("javax.annotation:javax.annotation-api:$annotationApiVersion")
 }
