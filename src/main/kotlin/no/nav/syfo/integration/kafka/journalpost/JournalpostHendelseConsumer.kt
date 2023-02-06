25a490903a6765ec40fcfad4d69fef55624697b4
Mikael Bjerga <mikael.bjerga@nav.no>
2023-02-06 11:36:09 +0100

    Logger journalposter som har mottakskanal NAV_NO.
    Prøver å finne ut om dette kan skje.
    
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>

--- src/main/kotlin/no/nav/syfo/integration/kafka/journalpost/JournalpostHendelseConsumer.kt
+++ src/main/kotlin/no/nav/syfo/integration/kafka/journalpost/JournalpostHendelseConsumer.kt
@@ -5,6 +5,7 @@ import no.nav.helse.arbeidsgiver.bakgrunnsjobb.Bakgrunnsjobb
 import no.nav.helse.arbeidsgiver.bakgrunnsjobb.BakgrunnsjobbRepository
 import no.nav.helse.arbeidsgiver.kubernetes.LivenessComponent
 import no.nav.helse.arbeidsgiver.kubernetes.ReadynessComponent
+import no.nav.helse.arbeidsgiver.utils.logger
 import no.nav.syfo.kafkamottak.InngaaendeJournalpostDTO
 import no.nav.syfo.prosesser.JoarkInntektsmeldingHendelseProsessor
 import no.nav.syfo.repository.DuplikatRepository
@@ -26,13 +27,14 @@ class JournalpostHendelseConsumer(
     private val om: ObjectMapper
 ) : ReadynessComponent, LivenessComponent {
 
-    private val log = LoggerFactory.getLogger(JournalpostHendelseConsumer::class.java)
+    private val logger = logger()
+    private val loggerSikker = LoggerFactory.getLogger("tjenestekall")
     private val consumer: KafkaConsumer<String, GenericRecord> = KafkaConsumer(props)
     private var ready = false
     private var error = false
 
     init {
-        log.info("Lytter på topic $topicName")
+        logger.info("Lytter på topic $topicName")
         consumer.subscribe(listOf(topicName))
     }
 
@@ -45,7 +47,7 @@ class JournalpostHendelseConsumer(
     }
 
     fun start() {
-        log.info("Starter...")
+        logger.info("Starter...")
         consumer.use {
             setIsReady(true)
             while (!error) {
@@ -56,7 +58,7 @@ class JournalpostHendelseConsumer(
                             processHendelse(mapJournalpostHendelse(record.value()))
                             it.commitSync()
                         } catch (e: Throwable) {
-                            log.error("Klarte ikke behandle hendelse. Stopper lytting!", e)
+                            logger.error("Klarte ikke behandle hendelse. Stopper lytting!", e)
                             setIsError(true)
                         }
                     }
@@ -66,9 +68,9 @@ class JournalpostHendelseConsumer(
 
     fun processHendelse(journalpostDTO: InngaaendeJournalpostDTO) {
         when (findStatus(journalpostDTO)) {
-            JournalpostStatus.Duplikat -> log.info("Ignorerer duplikat inntektsmelding ${journalpostDTO.kanalReferanseId} for hendelse ${journalpostDTO.hendelsesId}")
+            JournalpostStatus.Duplikat -> logger.info("Ignorerer duplikat inntektsmelding ${journalpostDTO.kanalReferanseId} for hendelse ${journalpostDTO.hendelsesId}")
             JournalpostStatus.Ny -> lagreBakgrunnsjobb(journalpostDTO)
-            JournalpostStatus.IkkeInntektsmelding -> log.info("Ignorerte journalposthendelse ${journalpostDTO.hendelsesId}. Kanal: ${journalpostDTO.mottaksKanal} Tema: ${journalpostDTO.temaNytt} Status: ${journalpostDTO.journalpostStatus}")
+            JournalpostStatus.IkkeInntektsmelding -> logger.info("Ignorerte journalposthendelse ${journalpostDTO.hendelsesId}. Kanal: ${journalpostDTO.mottaksKanal} Tema: ${journalpostDTO.temaNytt} Status: ${journalpostDTO.journalpostStatus}")
         }
     }
 
@@ -78,12 +80,17 @@ class JournalpostHendelseConsumer(
                 return JournalpostStatus.Duplikat
             }
             return JournalpostStatus.Ny
+        } else if (journalpostDTO.temaNytt == "SYK" && journalpostDTO.mottaksKanal == "NAV_NO" && journalpostDTO.journalpostStatus == "MOTTATT") {
+            "Fant journalpost som var [temaNytt=SYK, mottaksKanal=NAV_NO, journalpostStatus=MOTTATT].".let {
+                logger.info(it)
+                loggerSikker.info("$it journalpost: $journalpostDTO")
+            }
         }
         return JournalpostStatus.IkkeInntektsmelding
     }
 
     private fun lagreBakgrunnsjobb(hendelse: InngaaendeJournalpostDTO) {
-        log.info("Lagrer inntektsmelding ${hendelse.kanalReferanseId} for hendelse ${hendelse.hendelsesId}")
+        logger.info("Lagrer inntektsmelding ${hendelse.kanalReferanseId} for hendelse ${hendelse.hendelsesId}")
         bakgrunnsjobbRepo.save(
             Bakgrunnsjobb(
                 type = JoarkInntektsmeldingHendelseProsessor.JOB_TYPE,
@@ -111,6 +118,5 @@ class JournalpostHendelseConsumer(
     }
 }
 
-fun isInntektsmelding(hendelse: InngaaendeJournalpostDTO): Boolean {
-    return hendelse.temaNytt == "SYK" && hendelse.mottaksKanal == "ALTINN" && hendelse.journalpostStatus == "MOTTATT"
-}
+fun isInntektsmelding(hendelse: InngaaendeJournalpostDTO): Boolean =
+    hendelse.temaNytt == "SYK" && hendelse.mottaksKanal == "ALTINN" && hendelse.journalpostStatus == "MOTTATT"
