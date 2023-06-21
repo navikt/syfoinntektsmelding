ce69b0977d8cac6e7b485aa6c223662c5b9055fd
Gustav Berggren <gustav.berggren@nav.no>
2023-06-21 15:29:01 +0200

    legg til token support

--- src/main/kotlin/no/nav/syfo/web/InnteksmeldingModule.kt
+++ src/main/kotlin/no/nav/syfo/web/InnteksmeldingModule.kt
@@ -3,16 +3,24 @@ package no.nav.syfo.web
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.authenticate
 import io.ktor.config.ApplicationConfig
 import io.ktor.features.ContentNegotiation
 import io.ktor.http.ContentType
 import io.ktor.jackson.JacksonConverter
 import io.ktor.routing.route
 import io.ktor.routing.routing
+import no.nav.security.token.support.ktor.tokenValidationSupport
 import no.nav.syfo.web.api.syfoinntektsmelding
 import org.koin.ktor.ext.get
 
 fun Application.inntektsmeldingModule(config: ApplicationConfig = environment.config) {
+
+
+    install(Authentication) {
+        tokenValidationSupport(config = config)
+    }
     install(ContentNegotiation) {
         val commonObjectMapper = get<ObjectMapper>()
         register(ContentType.Application.Json, JacksonConverter(commonObjectMapper))
@@ -20,7 +28,9 @@ fun Application.inntektsmeldingModule(config: ApplicationConfig = environment.co
 
     routing {
         route("/api/v1") {
-            syfoinntektsmelding(get(), get(), get())
+            authenticate {
+                syfoinntektsmelding(get(), get(), get())
+            }
         }
     }
 }
