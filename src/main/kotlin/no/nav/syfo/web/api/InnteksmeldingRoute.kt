872333eaf69ce9f879c3fd052e999de648652ee3
Gustav Berggren <gustav.berggren@nav.no>
2023-06-21 12:54:56 +0200

    optimize imports

--- src/main/kotlin/no/nav/syfo/web/api/InnteksmeldingRoute.kt
+++ src/main/kotlin/no/nav/syfo/web/api/InnteksmeldingRoute.kt
@@ -8,11 +8,9 @@ package no.nav.syfo.web.api
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
-import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
-import io.ktor.routing.post
 import io.ktor.routing.route
 import no.nav.helsearbeidsgiver.utils.logger
 import no.nav.syfo.behandling.InntektsmeldingBehandler
