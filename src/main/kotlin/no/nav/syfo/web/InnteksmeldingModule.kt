0a6ef883f03feaa84ac0282026f5b12b2ef2e706
Gustav Berggren <gustav.berggren@nav.no>
2023-07-03 13:18:25 +0200

    sjekk at token har rollen application

--- src/main/kotlin/no/nav/syfo/web/InnteksmeldingModule.kt
+++ src/main/kotlin/no/nav/syfo/web/InnteksmeldingModule.kt
@@ -11,13 +11,21 @@ import io.ktor.http.ContentType
 import io.ktor.jackson.JacksonConverter
 import io.ktor.routing.route
 import io.ktor.routing.routing
+import no.nav.helse.arbeidsgiver.system.getString
 import no.nav.security.token.support.ktor.tokenValidationSupport
 import no.nav.syfo.web.api.syfoinntektsmelding
 import org.koin.ktor.ext.get
 
 fun Application.inntektsmeldingModule(config: ApplicationConfig = environment.config) {
     install(Authentication) {
-        tokenValidationSupport(config = config)
+        tokenValidationSupport(
+            config = config,
+            additionalValidation = {
+                val claims = it.getClaims(config.getString("no.nav.security.jwt.issuers[0].issuer_name"))
+                val roles = claims.getAsList("roles")
+                roles.size == 1 && roles.contains("access_as_application")
+            }
+        )
     }
     install(ContentNegotiation) {
         val commonObjectMapper = get<ObjectMapper>()
