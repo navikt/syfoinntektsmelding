116b7ba7d7174083b433bd29eed8249558937cb4
Morten Byhring <morten.byhring@nav.no>
2023-09-21 11:52:17 +0200

    refaktorering: -rename mapper-metoder: tilXML -> fraXML -fjernet ubrukt kode

--- src/main/kotlin/no/nav/syfo/mapping/FellesMapper.kt
+++ src/main/kotlin/no/nav/syfo/mapping/FellesMapper.kt
@@ -7,4 +7,3 @@ fun mapNaturalytelseType(naturalytelseType: JAXBElement<String>) =
     naturalytelseType.value?.let { n ->
         if (Naturalytelse.values().map { it.name }.contains(n.uppercase())) Naturalytelse.valueOf(n.uppercase()) else Naturalytelse.ANNET
     }
-        ?: Naturalytelse.ANNET
