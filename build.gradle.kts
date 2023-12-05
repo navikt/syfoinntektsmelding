adfa210ec262c1da7eb70afeffe16bd44a41fdb6
Mikael Bjerga <6940327+bjerga@users.noreply.github.com>
2023-12-05 11:57:24 +0100

    Erstatt utdatert kode i build-fil (#535)

--- build.gradle.kts
+++ build.gradle.kts
@@ -108,7 +108,7 @@ tasks.jar {
     }
     doLast {
         configurations.runtimeClasspath.get().forEach {
-            val file = File("$buildDir/libs/${it.name}")
+            val file = layout.buildDirectory.file("libs/${it.name}").get().asFile
             if (!file.exists())
                 it.copyTo(file)
         }
