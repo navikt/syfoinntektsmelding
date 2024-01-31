f146f511baa3a29fef00ade3e10a403c46e1f1b1
Gustav Berggren <gustav.berggren@nav.no>
2024-01-31 16:23:01 +0100

    fiks prodkoinprofile
    Co-authored-by: Mikael Bjerga <mikael.bjerga@nav.no>
    Co-authored-by: Morten Byhring <Morten.Byhring@nav.no>

--- src/main/kotlin/no/nav/syfo/koin/ProdKoinProfile.kt
+++ src/main/kotlin/no/nav/syfo/koin/ProdKoinProfile.kt
@@ -154,12 +154,6 @@ fun prodConfig(config: ApplicationConfig) = module {
     single {
         Norg2Client(
             config.getString("norg2_url"),
-            RestSTSAccessTokenProvider(
-                config.getString("security_token.username"),
-                config.getString("security_token.password"),
-                config.getString("security_token_service_token_url"),
-                get()
-            ),
             get()
         )
     } bind Norg2Client::class
