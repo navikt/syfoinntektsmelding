d8c3a0fcccaac565045c473c25359e4f8208db9a
Gustav Berggren <gustav.berggren@nav.no>
2024-09-09 15:37:08 +0200

    Bruk inline kode i InntektsmeldingConsumer

--- src/main/kotlin/no/nav/syfo/integration/kafka/UtsattOppgaveConsumer.kt
+++ src/main/kotlin/no/nav/syfo/integration/kafka/UtsattOppgaveConsumer.kt
@@ -26,8 +26,7 @@ class UtsattOppgaveConsumer(
     val om: ObjectMapper,
     val utsattOppgaveService: UtsattOppgaveService,
     val bakgrunnsjobbRepo: BakgrunnsjobbRepository,
-) : ReadynessComponent,
-    LivenessComponent {
+) : ReadynessComponent, LivenessComponent {
     private val consumer: KafkaConsumer<String, String> = KafkaConsumer(props, StringDeserializer(), StringDeserializer())
     private val logger = this.logger()
     private val sikkerlogger = sikkerLogger()
