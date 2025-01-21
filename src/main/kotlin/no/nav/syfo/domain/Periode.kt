d04a01acd150faadf9e1758dda56e23f2697d57a
Jesper Hustad <jesper@hustad.biz>
2025-01-21 12:51:06 +0100

    Mer beskrivende dokumentbeskrivelse (#618)
    * dokumentbeskrivelse med tester
    
    * lagt til tester
    
    * tilKortFormat returnerer nå null for tom input

--- src/main/kotlin/no/nav/syfo/domain/Periode.kt
+++ src/main/kotlin/no/nav/syfo/domain/Periode.kt
@@ -1,8 +1,22 @@
 package no.nav.syfo.domain
 
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 data class Periode(
     val fom: LocalDate,
     val tom: LocalDate,
 )
+
+val norskDatoFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+
+fun LocalDate.tilNorskFormat(): String = format(norskDatoFormat)
+
+fun List<Periode>.tilKortFormat(): String? =
+    if (isEmpty()) {
+        null
+    } else if (size == 1) {
+        "${first().fom.tilNorskFormat()} - ${first().tom.tilNorskFormat()}"
+    } else {
+        "${first().fom.tilNorskFormat()} - [...] - ${last().tom.tilNorskFormat()}"
+    }
