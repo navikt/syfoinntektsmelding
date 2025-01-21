98836e33e6bf6dc1074c1881b4b9d869673c892d
Jesper Hustad <jesper.forrest.hustad@nav.no>
2025-01-21 10:59:52 +0100

    tilKortFormat returnerer nå null for tom input

--- src/main/kotlin/no/nav/syfo/domain/Periode.kt
+++ src/main/kotlin/no/nav/syfo/domain/Periode.kt
@@ -12,10 +12,10 @@ val norskDatoFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy
 
 fun LocalDate.tilNorskFormat(): String = format(norskDatoFormat)
 
-fun List<Periode>.tilKortFormat(emptyArrayString: String): String =
+fun List<Periode>.tilKortFormat(): String? =
     if (isEmpty()) {
-        emptyArrayString
-    } else if (size < 2) {
+        null
+    } else if (size == 1) {
         "${first().fom.tilNorskFormat()} - ${first().tom.tilNorskFormat()}"
     } else {
         "${first().fom.tilNorskFormat()} - [...] - ${last().tom.tilNorskFormat()}"
--- src/main/kotlin/no/nav/syfo/util/DokumentbeskrivelseUtils.kt
+++ src/main/kotlin/no/nav/syfo/util/DokumentbeskrivelseUtils.kt
@@ -1,10 +1,11 @@
 package no.nav.syfo.util
 
+import no.nav.helsearbeidsgiver.utils.pipe.orDefault
 import no.nav.syfo.domain.inntektsmelding.Inntektsmelding
 import no.nav.syfo.domain.tilKortFormat
 
 fun Inntektsmelding.tilDokumentbeskrivelse(): String {
     val orgnr = this.arbeidsgiverOrgnummer.let { if (it.isNullOrBlank()) "(ingen orgnr)" else it }
-    val agp = this.arbeidsgiverperioder.tilKortFormat("(ingen agp)")
+    val agp = this.arbeidsgiverperioder.tilKortFormat().orDefault("(ingen agp)")
     return "Inntektsmelding-$orgnr-$agp"
 }
