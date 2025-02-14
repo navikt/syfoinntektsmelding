bd66d581b76c6a35d8ad60e1c6d5be3a49ee753f
Jesper Hustad <jesper@hustad.biz>
2025-02-14 12:09:31 +0100

    fjern orgnr dokarkiv tittel syfo (#623)
    * fjernet orgnr fra dokarkiv dokument tittel syfo
    
    * fikset feilende test

--- src/main/kotlin/no/nav/syfo/util/DokumentbeskrivelseUtils.kt
+++ src/main/kotlin/no/nav/syfo/util/DokumentbeskrivelseUtils.kt
@@ -5,7 +5,6 @@ import no.nav.syfo.domain.inntektsmelding.Inntektsmelding
 import no.nav.syfo.domain.tilKortFormat
 
 fun Inntektsmelding.tilDokumentbeskrivelse(): String {
-    val orgnr = this.arbeidsgiverOrgnummer.let { if (it.isNullOrBlank()) "(ingen orgnr)" else it }
     val agp = this.arbeidsgiverperioder.tilKortFormat().orDefault("(ingen agp)")
-    return "Inntektsmelding-$orgnr-$agp"
+    return "Inntektsmelding-$agp"
 }
