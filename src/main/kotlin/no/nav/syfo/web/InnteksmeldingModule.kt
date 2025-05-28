80c01842f774c99df68ab9b75cb11ff9a3904020
Mehdi.Zare <mehdi.zare@nav.no>
2025-05-28 06:56:05 +0200

    Legger til fnr i InntektsmeldingEntitet

--- src/main/kotlin/no/nav/syfo/web/InnteksmeldingModule.kt
+++ src/main/kotlin/no/nav/syfo/web/InnteksmeldingModule.kt
@@ -47,7 +47,7 @@ fun Application.inntektsmeldingModule(config: ApplicationConfig = environment.co
             authenticate {
                 route("/inntektsmelding") {
                     syfoinntektsmelding(get(), customObjectMapper())
-                    finnInntektsmeldinger(get(), customObjectMapper())
+                    finnInntektsmeldinger(get())
                 }
             }
         }
