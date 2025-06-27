211df9068d070460fa329270ff361bf3eb14245c
Jesper Hustad <jesper.forrest.hustad@nav.no>
2025-06-27 13:50:16 +0200

    InntektsmeldingConsumer håndter uten arbeidsforhold Opprett gosys oppgave i InntektsmeldingConsumer for im med ingen arbeidsforhold og ikke legg til im med ingen arbeidsforhold i topic

--- src/main/kotlin/no/nav/syfo/simba/InntektsmeldingConsumer.kt
+++ src/main/kotlin/no/nav/syfo/simba/InntektsmeldingConsumer.kt
@@ -89,13 +89,21 @@ class InntektsmeldingConsumer(
         }
     }
 
-    private fun behandle(
+    fun Inntektsmelding.Type.harIngenArbeidsforhold(): Boolean = this is Inntektsmelding.Type.Fisker || this is Inntektsmelding.Type.UtenArbeidsforhold
+
+    fun behandle(
         journalpostId: String,
         inntektsmeldingFraSimba: Inntektsmelding,
     ) {
         val aktorid = hentAktoeridFraPDL(inntektsmeldingFraSimba.sykmeldt.fnr.verdi)
         val arkivreferanse = "im_$journalpostId"
         val inntektsmelding = mapInntektsmelding(arkivreferanse, aktorid, journalpostId, inntektsmeldingFraSimba)
+        val timeout =
+            if (inntektsmeldingFraSimba.type.harIngenArbeidsforhold()) {
+                LocalDateTime.now() // opprett oppgave umiddelbart
+            } else {
+                LocalDateTime.now().plusHours(OPPRETT_OPPGAVE_FORSINKELSE)
+            }
         val dto = inntektsmeldingService.lagreBehandling(inntektsmelding, aktorid)
         utsattOppgaveService.opprett(
             UtsattOppgaveEntitet(
@@ -105,7 +113,7 @@ class InntektsmeldingConsumer(
                 arkivreferanse = inntektsmelding.arkivRefereranse,
                 inntektsmeldingId = dto.uuid,
                 tilstand = Tilstand.Utsatt,
-                timeout = LocalDateTime.now().plusHours(OPPRETT_OPPGAVE_FORSINKELSE),
+                timeout = timeout,
                 gosysOppgaveId = null,
                 oppdatert = null,
                 speil = false,
@@ -122,9 +130,11 @@ class InntektsmeldingConsumer(
                 dto.uuid,
             )
 
-        inntektsmeldingAivenProducer.leggMottattInntektsmeldingPåTopics(mappedInntektsmelding)
+        if (!inntektsmeldingFraSimba.type.harIngenArbeidsforhold()) {
+            inntektsmeldingAivenProducer.leggMottattInntektsmeldingPåTopics(mappedInntektsmelding)
 
-        sikkerlogger.info("Publiserte inntektsmelding på topic: $mappedInntektsmelding")
+            sikkerlogger.info("Publiserte inntektsmelding på topic: $mappedInntektsmelding")
+        }
     }
 
     private fun hentAktoeridFraPDL(identitetsnummer: String): String {
---
+++ src/test/kotlin/no/nav/syfo/simba/InntektsmeldingConsumerTest.kt
@@ -0,0 +1,86 @@
+package no.nav.syfo.simba
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
+import io.mockk.verify
+import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Inntektsmelding
+import no.nav.helsearbeidsgiver.pdl.PdlClient
+import no.nav.syfo.behandling.OPPRETT_OPPGAVE_FORSINKELSE
+import no.nav.syfo.integration.kafka.joarkLocalProperties
+import no.nav.syfo.producer.InntektsmeldingAivenProducer
+import no.nav.syfo.service.InntektsmeldingService
+import no.nav.syfo.utsattoppgave.UtsattOppgaveService
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import java.util.UUID
+
+internal class InntektsmeldingConsumerTest {
+    lateinit var consumer: InntektsmeldingConsumer
+    var props = joarkLocalProperties().toMap()
+    val inntektsmeldingService: InntektsmeldingService = mockk(relaxed = true)
+    val inntektsmeldingAivenProducer: InntektsmeldingAivenProducer = mockk(relaxed = true)
+    var utsattOppgaveService: UtsattOppgaveService = mockk(relaxed = true)
+    val pdlClient: PdlClient = mockk(relaxed = true)
+    val topicName = "helsearbeidsgiver.inntektsmelding"
+    val testNow: LocalDateTime = LocalDateTime.of(2025, 12, 5, 16, 45)
+
+    @BeforeEach
+    fun before() {
+        consumer = InntektsmeldingConsumer(props, topicName, inntektsmeldingService, inntektsmeldingAivenProducer, utsattOppgaveService, pdlClient)
+        mockkStatic(LocalDateTime::ofInstant)
+        every { LocalDateTime.now() } returns testNow
+    }
+
+    @AfterEach
+    fun after() {
+        unmockkStatic(LocalDateTime::ofInstant)
+    }
+
+    @Test
+    fun `behandle med IM type Forespurt opretter utsattOppgave med timeout inkludert forsinkelse og legger til IM på topic`() {
+        val timeoutNowPlusForsinkelse = LocalDateTime.now().plusHours(OPPRETT_OPPGAVE_FORSINKELSE)
+        val im = lagInntektsmelding()
+
+        consumer.behandle("123456789", im)
+
+        verify {
+            utsattOppgaveService.opprett(
+                match {
+                    it.timeout == timeoutNowPlusForsinkelse
+                },
+            )
+        }
+
+        verify(exactly = 1) {
+            inntektsmeldingAivenProducer.leggMottattInntektsmeldingPåTopics(any())
+        }
+    }
+
+    @Test
+    fun `behandle med IM fisker eller uten arbeidsforhold opretter utsattOppgave med timeout now og legger ikke til IM på topic`() {
+        val timeoutNow = LocalDateTime.now()
+        val imFisker = lagInntektsmelding().copy(type = Inntektsmelding.Type.Fisker(UUID.randomUUID()))
+        val imUtenArbeidsforhold = lagInntektsmelding().copy(type = Inntektsmelding.Type.UtenArbeidsforhold(UUID.randomUUID()))
+
+        listOf(imFisker, imUtenArbeidsforhold).forEachIndexed { index, im ->
+
+            consumer.behandle(index.toString(), im)
+
+            verify {
+                utsattOppgaveService.opprett(
+                    match {
+                        it.timeout == timeoutNow
+                    },
+                )
+            }
+
+            verify(exactly = 0) {
+                inntektsmeldingAivenProducer.leggMottattInntektsmeldingPåTopics(any())
+            }
+        }
+    }
+}
